e01082410501cd8e3c0d10afe7f3a4d855ac3c72
luke <daiagi@gmail.com>
2024-07-29 22:22:40 +0700

    use Blake2.hash2b

--- lib/util/hash.ex
+++ lib/util/hash.ex
@@ -7,10 +7,6 @@ defmodule Util.Hash do
   256 bits Blake2b hash function.
   """
   def blake2b_256(data) do
-    # Generate Blake2b hash with default output size (64 bytes)
-    full_hash = :crypto.hash(:blake2b, data)
-    # Truncate to 256 bits (32 bytes)
-    <<output::binary-size(32), _rest::binary>> = full_hash
-    output
+    Blake2.hash2b(data, 32)
   end
 end
