5d831ade9611d5828a8709a86961409e5d0f1cdc
luke <daiagi@gmail.com>
2024-07-31 17:01:17 +0700

    impl Entropy Accumulation

--- lib/util/time.ex
+++ lib/util/time.ex
@@ -1,6 +1,7 @@
 defmodule Util.Time do
   @epoch :calendar.datetime_to_gregorian_seconds({{2024, 1, 1}, {12, 0, 0}})
   @block_duration 6
+  @epoch_duration 600
 
   @doc """
   Returns the base epoch time in Gregorian seconds.
@@ -14,6 +15,13 @@ defmodule Util.Time do
   """
   def block_duration do
     @block_duration
+  end
+
+  @doc """
+  Returns the epoch duration in blocks.
+  """
+  def epoch_duration do
+    @epoch_duration
   end
 
   @doc """
@@ -37,5 +45,19 @@ defmodule Util.Time do
   def valid_block_timeslot?(block_timeslot) do
     block_time = block_timeslot * @block_duration
     valid_block_time?(block_time)
+  end
+
+  @doc """
+  Determines if a new epoch has started based on the previous and current timeslots.
+  """
+  def new_epoch?(previous_timeslot, current_timeslot) do
+    if previous_timeslot >= current_timeslot do
+      {:error,
+       "Invalid timeslot order: previous_timeslot (#{previous_timeslot}) is not less than current_timeslot (#{current_timeslot})"}
+    else
+      previous_epoch = div(previous_timeslot, @epoch_duration)
+      current_epoch = div(current_timeslot, @epoch_duration)
+      {:ok, current_epoch > previous_epoch}
+    end
   end
 end
--- test/util/time_test.exs
+++ test/util/time_test.exs
@@ -29,4 +29,29 @@ defmodule Util.TimeTest do
     future_block_timeslot = div(Time.current_time() + 10, Time.block_duration())
     refute Time.valid_block_timeslot?(future_block_timeslot)
   end
+
+  test "new epoch detection when crossing epoch boundary" do
+    previous_timeslot = 599
+    current_timeslot = 600
+    assert Time.new_epoch?(previous_timeslot, current_timeslot) == {:ok, true}
+  end
+
+  test "new epoch detection when within the same epoch" do
+    previous_timeslot = 100
+    current_timeslot = 200
+    assert Time.new_epoch?(previous_timeslot, current_timeslot) == {:ok, false}
+  end
+
+  test "new epoch detection with large jump across epochs" do
+    previous_timeslot = 599
+    current_timeslot = 1200
+    assert Time.new_epoch?(previous_timeslot, current_timeslot) == {:ok, true}
+  end
+
+  test "new epoch detection when previous_timeslot is greater than current_timeslot" do
+    previous_timeslot = 600
+    current_timeslot = 599
+
+    assert {:error, _error_msg} = Time.new_epoch?(previous_timeslot, current_timeslot)
+  end
 end
