7d0509494e8701aef6296725da7b0d063bfd4d56
luke <daiagi@gmail.com>
2024-08-02 13:46:57 +0700

    imports

--- lib/system/state.ex
+++ lib/system/state.ex
@@ -3,6 +3,8 @@ defmodule System.State do
   alias System.State.{Safrole, RecentBlock,Validator, Judgements}
   alias Disputes
 
+  alias System.State.{Validator, Judgements}
+
   @type t :: %__MODULE__{
           authorization_requirements: list(AuthorizationRequirement.t()),
           recent_blocks: list(RecentBlock.t()),
