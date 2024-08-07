191ae490aba383f52855329733f0def6a2b7b53e
luke <daiagi@gmail.com>
2024-08-07 19:28:25 +0700

    define more structs

--- lib/block/extrinsic/extrinsic.ex
+++ lib/block/extrinsic/extrinsic.ex
@@ -1,27 +1,31 @@
 defmodule Block.Extrinsic do
   alias Disputes
   # Equation (14)
-  defstruct tickets: [], disputes: nil, preimages: [], availability: [], reports: []
+  defstruct tickets: [], disputes: nil, preimages: [], availability: [], guarantees: []
+
+  alias Block.Extrinsics.{Guarantee}
+
 
   @type t :: %__MODULE__{
           tickets: list(Ticket.t()),
           disputes: Disputes.t(),
           preimages: list(Preimage.t()),
           availability: list(Availability.t()),
-          reports: list(Report.t())
+          # Eg
+          guarantees: list(Guarantee.t())
         }
 
   @doc """
   Represents the block extrinsic as described.
   E ≡ (ET, ED, EP, EA, EG)
   """
-  def new(tickets, disputes, preimages, availability, reports) do
+  def new(tickets, disputes, preimages, availability, guarantees) do
     %Block.Extrinsic{
       tickets: tickets,
       disputes: disputes,
       preimages: preimages,
       availability: availability,
-      reports: reports
+      guarantees: guarantees
     }
   end
 end
---
+++ lib/system/work_result.ex
@@ -0,0 +1,26 @@
+defmodule System.WorkResult do
+  @moduledoc """
+  A module representing the result of a work-package, including the details of the service state alteration, code hash, payload hash, gas prioritization ratio, and output or error.
+  """
+
+  @type error :: :out_of_gas | :unexpected_termination | :bad_code | :code_too_large
+
+  @type t :: %__MODULE__{
+          # s: the index of the service whose state is to be altered and thus whose refine code was already executed
+          service_index: non_neg_integer(),
+          # c: hash of the code of the service at the time of being reported
+          code_hash: <<_::256>>,
+          # l: the hash of the payload (l) within the work item which was executed in the refine stage to give this result
+          payload_hash: <<_::256>>,
+          # g: the gas prioritization ratio used when determining how much gas should be allocated to execute this item’s accumulate
+          gas_prioritization_ratio: non_neg_integer(),
+          # o: the output or error of the execution of the code, which may be either an octet sequence in case it was successful, or a member of the set J if not
+          output_or_error: binary() | error()
+        }
+
+  defstruct service_index: 0,
+            code_hash: <<0::256>>,
+            payload_hash: <<0::256>>,
+            gas_prioritization_ratio: 0,
+            output_or_error: <<>>
+end
