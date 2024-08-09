27b939392d73df5a6836c51d4bfca4b08703624d
Daniel Cukier <danicuki@gmail.com>
2024-08-09 16:15:09 +0100

    implemented integer encoding

--- test/codec/encoder_test.exs
+++ test/codec/encoder_test.exs
@@ -1,8 +1,26 @@
 defmodule CodecEncoderTest do
   use ExUnit.Case
-  import Bitwise
 
   alias Codec.Encoder
+
+  describe "encode_integer/1" do
+    test "encode integer 0" do
+      assert Encoder.encode(0) == <<0>>
+    end
+
+    # Equation (273)
+    test "encode integer < 2^64" do
+      assert Encoder.encode(2 ** 2) == <<4>>
+      assert Encoder.encode(2 ** 8) == <<129, 0>>
+      assert Encoder.encode(2 ** 8 + 1) == <<129, 1>>
+      assert Encoder.encode(2 ** 16) == <<193, 0, 0>>
+      assert Encoder.encode(2 ** 64 - 1) == <<255, 255, 255, 255, 255, 255, 255, 255, 255>>
+    end
+
+    test "encode integer >= 2^64" do
+      assert_raise ArgumentError, fn -> Encoder.encode(2 ** 64) end
+    end
+  end
 
   describe "encode/1" do
     test "encode empty sequence" do
@@ -17,48 +35,12 @@ defmodule CodecEncoderTest do
 
     test "encode tuple" do
       assert Encoder.encode({1, 2, 3}) == <<1, 2, 3>>
-      assert Encoder.encode({127, 101}) == <<127, 101>>
     end
 
-    test "encode integers from 0 to 127" do
-      assert Encoder.encode(0) == <<0>>
-      assert Encoder.encode(63) == <<63>>
-      assert Encoder.encode(127) == <<127>>
-    end
-
-    test "encode integers from 2^7 to 2^14 (not inclusive)" do
-      assert Encoder.encode(1 <<< 7) == <<128, 128>>
-      assert Encoder.encode((1 <<< 7) + 1) == <<128, 129>>
-      assert Encoder.encode(1 <<< 8) == <<129, 0>>
-      assert Encoder.encode((1 <<< 8) + 1) == <<129, 1>>
-      assert Encoder.encode((1 <<< 14) - 1) == <<191, 255>>
-    end
-
-    test "encode integers from 2^14 to 2^21 (not inclusive)" do
-      assert Encoder.encode(1 <<< 14) == <<192, 0, 64>>
-      assert Encoder.encode((1 <<< 14) + 1) == <<192, 1, 64>>
-      assert Encoder.encode((1 <<< 16) - 1) == <<192, 255, 255>>
-      assert Encoder.encode(1 <<< 16) == <<193, 0, 0>>
-      assert Encoder.encode((1 <<< 21) - 1) == <<223, 255, 255>>
-    end
-
-    test "encode integers from 2^21 to 2^28 (not inclusive)" do
-      assert Encoder.encode(1 <<< 21) == <<224, 0, 0, 32>>
-      assert Encoder.encode((1 <<< 21) + 1) == <<224, 1, 0, 32>>
-      assert Encoder.encode(1 <<< 24) == <<225, 0, 0, 0>>
-      assert Encoder.encode((1 <<< 24) + 1) == <<225, 1, 0, 0>>
-      assert Encoder.encode((1 <<< 28) - 1) == <<239, 255, 255, 255>>
-    end
-
-    test "encode integers from 2^28 to 2^64 (not inclusive)" do
-      assert Encoder.encode(1 <<< 28) == <<255, 0, 0, 0, 16, 0, 0, 0, 0>>
-      assert Encoder.encode((1 <<< 28) + 1) == <<255, 1, 0, 0, 16, 0, 0, 0, 0>>
-      assert Encoder.encode((1 <<< 29) - 1) == <<255, 255, 255, 255, 31, 0, 0, 0, 0>>
-      assert Encoder.encode(1 <<< 29) == <<255, 0, 0, 0, 32, 0, 0, 0, 0>>
-      assert Encoder.encode((1 <<< 29) + 1) == <<255, 1, 0, 0, 32, 0, 0, 0, 0>>
-      assert Encoder.encode(1 <<< 30) == <<255, 0, 0, 0, 64, 0, 0, 0, 0>>
-      assert Encoder.encode(1 <<< 31) == <<255, 0, 0, 0, 128, 0, 0, 0, 0>>
-      assert Encoder.encode((1 <<< 64) - 1) == <<255, 255, 255, 255, 255, 255, 255, 255, 255>>
+    test "encode tuple with big integers" do
+      left = Encoder.encode({2 ** 16, 2 ** 8})
+      right = Encoder.encode(2 ** 16) <> Encoder.encode(2 ** 8)
+      assert left == right
     end
 
     test "encode nil" do
