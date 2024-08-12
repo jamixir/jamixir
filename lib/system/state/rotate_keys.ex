e74e5b418a01f5bd4d9dbb723dc45b34f0c525a8
luke <daiagi@gmail.com>
2024-08-12 15:45:06 +0700

    rotate keys

---
+++ lib/system/state/rotate_keys.ex
@@ -0,0 +1,85 @@
+defmodule System.State.RotateKeys do
+  alias Util.{Time, Crypto}
+  alias Block.Header
+
+  alias System.State.{
+    Validator,
+    Judgements,
+    Safrole
+  }
+
+  # Equation (58)
+  @spec rotate_keys(
+          Header.t(),
+          integer(),
+          list(Validator.t()),
+          list(Validator.t()),
+          list(Validator.t()),
+          Safrole.t(),
+          Judgements.t()
+        ) ::
+          {list(Validator.t()), list(Validator.t()), list(Validator.t()),
+           Types.bandersnatch_ring_root()}
+
+  def rotate_keys(
+        %Header{timeslot: new_timeslot},
+        timeslot,
+        prev_validators,
+        curr_validators,
+        next_validators,
+        %Safrole{pending: pending, epoch_root: epoch_root},
+        %Judgements{punish: offendsers}
+      ) do
+    case Time.new_epoch?(timeslot, new_timeslot) do
+      {:ok, true} ->
+        # Equation (58) -  new epoch - rotate keys
+        # {γ_k', κ', λ', γ_z'} = {Φ(ι), γ_k, κ, z}
+
+        # next -> penfing
+        # γ_k' = Φ(ι)
+        new_pending = nullify_offenders(next_validators, offendsers)
+        # penfing -> current
+        # κ' = γ_k
+        new_current = pending
+        # current -> prev
+        # λ' = κ
+        new_prev = curr_validators
+
+        # γ_z' = z, z = O([kb ∣ k <- γk ])
+        new_epoch_root = Crypto.bandersnatch_ring_root(Enum.map(new_pending, & &1.bandersnatch))
+
+        {new_pending, new_current, new_prev, new_epoch_root}
+
+      {:ok, false} ->
+        # Equation (59) -  same epoch - no rotation
+        # {γ_k', κ', λ', γ_z'} = {γ_k, κ, λ, γ_z}
+        {pending, curr_validators, prev_validators, epoch_root}
+
+      {:error, reason} ->
+        raise "Error determining new epoch: #{reason}"
+    end
+  end
+
+  # Equation (59)
+  @spec nullify_offenders(
+          list(Validator.t()),
+          MapSet.t(Types.ed25519_key())
+        ) :: list(Validator.t())
+  def nullify_offenders(
+        next_validators = [%System.State.Validator{}],
+        offenders
+      ) do
+    Enum.map(next_validators, fn %Validator{} = validator ->
+      if MapSet.member?(offenders, validator.ed25519) do
+        %Validator{
+          bandersnatch: <<0::size(bit_size(validator.bandersnatch))>>,
+          ed25519: <<0::size(bit_size(validator.ed25519))>>,
+          bls: <<0::size(bit_size(validator.bls))>>,
+          metadata: <<0::size(bit_size(validator.metadata))>>
+        }
+      else
+        validator
+      end
+    end)
+  end
+end
