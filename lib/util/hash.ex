e2752d1bf8539d35c53b2a574b9a0829c3f88719
Daniel Cukier <danicuki@gmail.com>
2024-08-13 15:36:14 +0100

    refactor

--- lib/util/hash.ex
+++ lib/util/hash.ex
@@ -11,11 +11,10 @@ defmodule Util.Hash do
   @doc """
   256 bits Blake2b hash function.
   """
-  def blake2b_256(data) do
-    Blake2.hash2b(data, 32)
-  end
+  def blake2b_256(data), do: Blake2.hash2b(data, 32)
 
-  def keccak_256(data) do
-    ExKeccak.hash_256(data)
-  end
+  @doc """
+  256 bits keccak hash function.
+  """
+  def keccak_256(data), do: ExKeccak.hash_256(data)
 end
