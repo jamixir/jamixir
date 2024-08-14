caab95394564e968f9ed04ed8468b258b4ebaa83
luke <daiagi@gmail.com>
2024-08-14 19:51:12 +0700

    cleaner encode decode test code

--- test/codec/encode_decode_test.exs
+++ test/codec/encode_decode_test.exs
@@ -4,59 +4,24 @@ defmodule Codec.EncodeDecodeTest do
 
   describe "encode_le/2 and decode_le/2" do
     test "encode and decode returns the original value" do
-      value = 0
-      length = 1
-      encoded = Encoder.encode_le(value, length)
-      decoded = Decoder.decode_le(encoded, length)
-      assert decoded == value
+      test_cases = [
+        {0, 1},
+        {1, 1},
+        {255, 1},
+        {256, 2},
+        {257, 2},
+        {65535, 2},
+        {65536, 3},
+        {16_777_215, 3},
+        {4_294_967_295, 4}
+      ]
 
-      value = 1
-      length = 1
-      encoded = Encoder.encode_le(value, length)
-      decoded = Decoder.decode_le(encoded, length)
-      assert decoded == value
+      Enum.each(test_cases, fn {value, size} ->
+        encoded = Encoder.encode_le(value, size)
+        decoded = Decoder.decode_le(encoded, size)
 
-      value = 255
-      length = 1
-      encoded = Encoder.encode_le(value, length)
-      decoded = Decoder.decode_le(encoded, length)
-      assert decoded == value
-
-      value = 256
-      length = 2
-      encoded = Encoder.encode_le(value, length)
-      decoded = Decoder.decode_le(encoded, length)
-      assert decoded == value
-
-      value = 257
-      length = 2
-      encoded = Encoder.encode_le(value, length)
-      decoded = Decoder.decode_le(encoded, length)
-      assert decoded == value
-
-      value = 65535
-      length = 2
-      encoded = Encoder.encode_le(value, length)
-      decoded = Decoder.decode_le(encoded, length)
-      assert decoded == value
-
-      value = 65536
-      length = 3
-      encoded = Encoder.encode_le(value, length)
-      decoded = Decoder.decode_le(encoded, length)
-      assert decoded == value
-
-      value = 16777215
-      length = 3
-      encoded = Encoder.encode_le(value, length)
-      decoded = Decoder.decode_le(encoded, length)
-      assert decoded == value
-
-      value = 4294967295
-      length = 4
-      encoded = Encoder.encode_le(value, length)
-      decoded = Decoder.decode_le(encoded, length)
-      assert decoded == value
+        assert decoded == value, "Failed for value: #{value}"
+      end)
     end
   end
 end
