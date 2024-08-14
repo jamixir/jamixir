8611e3377a9df24fdf4a7b82a67b0162f7a3c401
luke <daiagi@gmail.com>
2024-08-14 15:43:16 +0700

    ringVRF related types

--- lib/types.ex
+++ lib/types.ex
@@ -12,4 +12,22 @@ defmodule Types do
   @type validator_index :: non_neg_integer()
   @type epoch_index :: non_neg_integer()
   @type decision :: boolean()
+  # 144 bytes YR ⊂ Y144
+  @type bandersnatch_ring_root :: <<_::1152>>
+
+  @typedoc """
+  Fm∈Yk∈HB ⟨x ∈ Y⟩ ⊂ Y96
+   valid singly-contextualized signatures of utilizing the se-
+   cret counterpart to the public(Bandersnatch) key k, some context x and
+   message m
+  """
+  @type bandersnatch_signature :: <<_::768>>
+
+  @typedoc """
+  F̄m∈Yr∈YR ⟨x ∈ Y⟩ ⊂ Y784 is the set of valid Ban-
+  dersnatch Ringvrf deterministic singly-contextualized
+  proofs of knowledge of a secret within some set of secrets
+  identified by some root in the set of valid roots YR ⊂ Y144 .
+  """
+  @type bandersnatch_ringVRF_proof_of_knowledge :: <<_::6272>>
 end
