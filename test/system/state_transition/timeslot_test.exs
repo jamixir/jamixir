61a098debbe9dedb61c9a3dbd830388c2541f2a0
luke <daiagi@gmail.com>
2024-08-16 15:42:21 +0700

    break state_test into more managable pieces

---
+++ test/system/state_transition/timeslot_test.exs
@@ -0,0 +1,21 @@
+defmodule System.StateTransition.TimeslotTest do
+  use ExUnit.Case
+
+  alias System.State
+  alias Block.{Header}
+  alias Block
+
+  test "add_block/2 correctly sets timeslot" do
+    state = %State{
+      entropy_pool: %State.EntropyPool{current: "initial_entropy", history: ["eta1", "eta2", "eta3"]},
+      timeslot: 6
+    }
+
+    block = %Block{
+      header: %Header{timeslot: 7, vrf_signature: "0x00000000000"},
+      extrinsic: %Block.Extrinsic{}
+    }
+
+    assert State.add_block(state, block).timeslot === 7
+  end
+end
