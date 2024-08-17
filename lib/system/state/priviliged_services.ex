076515d5195686abe67d2b43101e1bdcdf7923f9
luke <daiagi@gmail.com>
2024-08-17 15:36:06 +0700

    impl Formula 98 - Priviliged Services

---
+++ lib/system/state/priviliged_services.ex
@@ -0,0 +1,21 @@
+defmodule System.State.PriviligedServices do
+  @moduledoc """
+  Formula (96) v0.3.4
+
+  Up to three services may be recognized as privileged, each with a specific role:
+
+  - `manager_service` (χm): The index of the manager service, which can alter the service privileges.
+  - `alter_authorizer_service` (χa): The index of the service able to alter the authorizer queue (φ).
+  - `alter_validator_service` (χv): The index of the service able to alter the validator queue (ι).
+  """
+
+  @type t :: %__MODULE__{
+          manager_service: non_neg_integer(),
+          alter_authorizer_service: non_neg_integer(),
+          alter_validator_service: non_neg_integer()
+        }
+
+  defstruct manager_service: 0,
+            alter_authorizer_service: 0,
+            alter_validator_service: 0
+end
