96c3d6d62774b854cc87a25399867cd85f50fd9f
luke <daiagi@gmail.com>
2024-08-19 14:45:10 +0700

    CARGO_MANIFEST_DIR env var

--- lib/mix/tasks/mix.jam.ex
+++ lib/mix/tasks/mix.jam.ex
@@ -4,6 +4,10 @@ defmodule Mix.Tasks.Jam do
   @shortdoc "Overrides the default `mix run` to start Jamixir.CLI"
 
   def run(_args) do
+    # Set CARGO_MANIFEST_DIR environment variable
+    manifest_dir = Path.expand("./native/bandersnatch_ring_vrf", Mix.Project.app_path())
+    System.put_env("CARGO_MANIFEST_DIR", manifest_dir)
+    IO.puts("CARGO_MANIFEST_DIR set to #{manifest_dir}")
     Jamixir.CLI.start()
   end
 end
