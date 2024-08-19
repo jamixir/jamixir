c921a50eb828d97608a3a7c61e10ce7efd9f6217
luke <daiagi@gmail.com>
2024-08-19 16:28:50 +0700

    got  data loading to work

--- lib/util/ring_vrf.ex
+++ lib/util/ring_vrf.ex
@@ -1,6 +1,19 @@
 defmodule BandersnatchRingVrf do
   use Rustler, otp_app: :jamixir, crate: :bandersnatch_ring_vrf
 
+  # These functions correspond to the NIFs defined in Rust
+  def create_ring_context(_file_contents), do: :erlang.nif_error(:nif_not_loaded)
   def create_verifier(_keys), do: :erlang.nif_error(:nif_not_loaded)
 
+  def init_ring_context() do
+    current_dir = File.cwd!()
+
+    filename =
+      Path.join([current_dir, "native/bandersnatch_ring_vrf/data/zcash-srs-2-11-uncompressed.bin"])
+
+    {:ok, file_contents} = File.read(filename)
+    # cobver from binary to list
+    file_contents = :binary.bin_to_list(file_contents)
+    create_ring_context(file_contents)
+  end
 end
