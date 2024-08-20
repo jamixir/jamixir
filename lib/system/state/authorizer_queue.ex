9d6fc41c4d8e42792b01d95eeb98cf9dc194bb2c
Noah Joeris <noahjoeris@gmail.com>
2024-08-20 21:28:31 +0300

    doc fix

--- lib/system/state/authorizer_queue.ex
+++ lib/system/state/authorizer_queue.ex
@@ -2,7 +2,7 @@ defmodule System.State.AuthorizerQueue do
   @moduledoc """
   Formula (85) v0.3.4
   Represents the state of the authorizer queue.
-  α ∈ ⟦⟦H⟧∶Q⟧C, where H is the authorizers hash. Q is the size of the authorizations queue (max_authorization_queue_items).
+  φ ∈ ⟦⟦H⟧∶Q⟧C, where H is the authorizers hash. Q is the size of the authorizations queue (max_authorization_queue_items).
   And C is the number of cores (core_count).
   """
 
