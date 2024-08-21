00b3c8c4ee261a9c38e10840932e3c57f0684833
luke <daiagi@gmail.com>
2024-08-21 20:44:54 +0700

    serlialize

---
+++ lib/util/ringCommitement.ex
@@ -0,0 +1,6 @@
+defmodule RingCommitment do
+  @enforce_keys [:points, :ring_selector]
+  defstruct points: [], ring_selector: nil, phantom: nil
+
+  # Add any additional functions or behaviors as needed
+end
