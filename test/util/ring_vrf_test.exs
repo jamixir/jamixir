79948f2d27cd5d6f3df2857ffdb6e3472be4d932
luke <daiagi@gmail.com>
2024-08-25 19:34:59 +0700

    binary instead of vec<u8>

--- test/util/ring_vrf_test.exs
+++ test/util/ring_vrf_test.exs
@@ -225,8 +225,8 @@ defmodule RingVrfTest do
     prover_idx = 0
 
     # Mock VRF input data and auxiliary data
-    vrf_input_data = "input data" |> :binary.bin_to_list()
-    aux_data = "aux data" |> :binary.bin_to_list()
+    vrf_input_data = "input data"
+    aux_data = "aux data"
     # Sign the data using the secret and the ring
     signature =
       BandersnatchRingVrf.ring_vrf_sign(keys, secret, prover_idx, vrf_input_data, aux_data)
@@ -240,22 +240,22 @@ defmodule RingVrfTest do
 
     # Assert that verification returns true
     assert verified
-    assert length(vrf_output_hash) == 32
+    assert byte_size(vrf_output_hash) == 32
   end
 
   describe "test secret generation" do
     test "generate_secret_from_seed generates a secret from a seed" do
       seed = :crypto.strong_rand_bytes(32) |> :binary.bin_to_list()
-      secret = BandersnatchRingVrf.generate_secret_from_seed(seed)
+      _secret = BandersnatchRingVrf.generate_secret_from_seed(seed)
     end
 
     test "generate_secret_from_rand generates a secret from randomness" do
-      secret = BandersnatchRingVrf.generate_secret_from_rand()
+      _secret = BandersnatchRingVrf.generate_secret_from_rand()
     end
 
     test "generate_secret_from_scalar generates a secret from a scalar" do
       scalar = :crypto.strong_rand_bytes(32) |> :binary.bin_to_list()
-      secret = BandersnatchRingVrf.generate_secret_from_scalar(scalar)
+      _secret = BandersnatchRingVrf.generate_secret_from_scalar(scalar)
     end
   end
 end
