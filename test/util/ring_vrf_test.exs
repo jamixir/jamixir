68dbe6eed33c31299be962feb8859485725e47a4
luke <daiagi@gmail.com>
2024-08-25 15:54:09 +0700

    full circle  sign -> verify works

--- test/util/ring_vrf_test.exs
+++ test/util/ring_vrf_test.exs
@@ -250,6 +250,49 @@ defmodule RingVrfTest do
     end
   end
 
+  test "end-to-end VRF signing and verification" do
+    # Initialize the ring context
+    BandersnatchRingVrf.init_ring_context(2)
+
+    # Generate a secret key from randomness
+    secret = BandersnatchRingVrf.generate_secret_from_scalar([1, 2, 3])
+    public = elem(secret, 1)
+
+    # Generate a ring of public keys with the public key derived from the secret at index 0
+    keys =
+      for _i <- 1..2 do
+        secret = BandersnatchRingVrf.generate_secret_from_rand()
+        elem(secret, 1)
+      end
+
+    # Prepend the public key to the list of keys
+    keys = [public | keys]
+    IO.inspect(keys, label: "Keys")
+
+    # Create a verifier (commitment)
+    commitment = BandersnatchRingVrf.create_commitment(keys)
+    # print commitment
+    IO.inspect(commitment, label: "Commitment")
+
+    # Select the prover index (for simplicity, use the first key)
+    prover_idx = 0
+
+    # Mock VRF input data and auxiliary data
+    vrf_input_data = "input data" |> :binary.bin_to_list()
+    aux_data = "aux data" |> :binary.bin_to_list()
+    # Sign the data using the secret and the ring
+    signature =
+      BandersnatchRingVrf.ring_vrf_sign(keys, secret, prover_idx, vrf_input_data, aux_data)
+
+    # Verify the signature using the commitment and the same input/aux data
+    vrf_output_hash =
+      BandersnatchRingVrf.ring_vrf_verify(commitment, vrf_input_data, aux_data, signature)
+
+    IO.inspect(vrf_output_hash, label: "VRF output hash")
+    # Assert that verification returns true
+    assert vrf_output_hash != nil
+  end
+
   describe "Context influence on output, message doesn't " do
     test "ring signature output changes with context but not with message" do
       {keys, secret} = init_ring_context_and_gen_keys(4)
