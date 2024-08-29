22b5a770950d3a693c02eec65a589fcdf6455120
luke <daiagi@gmail.com>
2024-08-29 18:08:21 +0700

    remove dead code

--- lib/util/bandersnatch.ex
+++
@@ -1,9 +0,0 @@
-defmodule Util.Bandersnatch do
-  def _verify(_public_key, _input_data, _aux_data, _signature),
-    # do: :erlang.nif_error(:nif_not_loaded)
-    do: {:ok, 0x01}
-
-  def _sign(_secret_key, _input_data, _aux_data),
-    # do: :erlang.nif_error(:nif_not_loaded)
-    do: "signature"
-end
