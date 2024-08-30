d4cf9ad39498f54daff873b300bbfb5014991391
luke <daiagi@gmail.com>
2024-08-30 18:20:55 +0700

    better key pair interface

--- native/bandersnatch_ring_vrf/src/secret_ops.rs
+++ native/bandersnatch_ring_vrf/src/secret_ops.rs
@@ -5,26 +5,29 @@ use rustler::NifResult;
 
 use ark_ec_vrfs::prelude::ark_ff::PrimeField;
 
-use crate::rustler_bridges::SecretBridge;
+use crate::rustler_bridges::{PublicBridge, SecretBridge};
 
 type S = bandersnatch::BandersnatchSha512Ell2;
 
 #[rustler::nif]
-fn generate_secret_from_seed(seed: Vec<u8>) -> NifResult<SecretBridge<S>> {
+fn generate_secret_from_seed(seed: Vec<u8>) -> NifResult<(SecretBridge<S>, PublicBridge<S>)> {
     let secret = Secret::from_seed(&seed);
-    Ok(secret.into())
+    let public = secret.public();
+    Ok((secret.into(), public.into()))
 }
 
 #[rustler::nif]
-fn generate_secret_from_rand() -> NifResult<SecretBridge<S>> {
+fn generate_secret_from_rand() -> NifResult<(SecretBridge<S>, PublicBridge<S>)> {
     let mut rng = rand_chacha::ChaCha20Rng::from_entropy();
     let secret = Secret::from_rand(&mut rng);
-    Ok(secret.into())
+    let public = secret.public();
+    Ok((secret.into(), public.into()))
 }
 
 #[rustler::nif]
-fn generate_secret_from_scalar(scalar_bytes: Vec<u8>) -> NifResult<SecretBridge<S>> {
+fn generate_secret_from_scalar(scalar_bytes: Vec<u8>) -> NifResult<(SecretBridge<S>, PublicBridge<S>)> {
     let scalar = ScalarField::<S>::from_le_bytes_mod_order(&scalar_bytes[..]);
     let secret = Secret::from_scalar(scalar);
-    Ok(secret.into())
+    let public = secret.public();
+    Ok((secret.into(), public.into()))
 }
