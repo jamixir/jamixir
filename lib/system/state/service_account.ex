95027f78b1171e7b0769c70f37458efbb290740b
Daniel Cukier <danicuki@gmail.com>
2024-09-04 14:37:45 +0100

    implemented historical storage lookup.

--- lib/system/state/service_account.ex
+++ lib/system/state/service_account.ex
@@ -68,6 +68,27 @@ defmodule System.State.ServiceAccount do
       Constants.additional_minimum_balance_per_octet() * octets_in_storage(sa)
   end
 
+  # Formula (94) v0.3.4
+  @spec historical_lookup(System.State.ServiceAccount.t(), integer(), Types.hash()) :: binary()
+  def historical_lookup(
+        %__MODULE__{preimage_storage_p: ap, preimage_storage_l: al},
+        timeslot,
+        hash
+      ) do
+    case Map.get(ap, hash) do
+      value ->
+        if value != nil and in_storage?(Map.get(al, {hash, length(value)}), timeslot),
+          do: value,
+          else: nil
+    end
+  end
+
+  defp in_storage?(nil, _), do: false
+  defp in_storage?([], _), do: false
+  defp in_storage?([x], t), do: x <= t
+  defp in_storage?([x, y], t), do: x <= t and t < y
+  defp in_storage?([x, y, z], t), do: (x <= t and t < y) or z <= t
+
   defimpl Encodable do
     alias Codec.Encoder
     alias System.State.ServiceAccount
