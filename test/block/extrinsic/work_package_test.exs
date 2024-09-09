c5d7f7f7112c328abb8d758ce70b616da28154f9
Daniel Cukier <danicuki@gmail.com>
2024-09-09 09:04:22 +0100

    fix bug - using correct blob_hashes_and_lengths field to calculate size

--- test/block/extrinsic/work_package_test.exs
+++ test/block/extrinsic/work_package_test.exs
@@ -34,8 +34,17 @@ defmodule WorkPackageTest do
     end
 
     test "validates different work_item imported_data_segments content size", %{wp: wp} do
-      medium_work_item = build(:work_item, imported_data_segments: [{<<0::256>>, 10_000_000}])
-      big_work_item = build(:work_item, imported_data_segments: [{<<0::256>>, 20_000_000}])
+      medium_work_item =
+        build(:work_item,
+          imported_data_segments: [{<<0::256>>, 10_000_000}],
+          blob_hashes_and_lengths: [{<<0::256>>, 10_000_000}]
+        )
+
+      big_work_item =
+        build(:work_item,
+          imported_data_segments: [{<<0::256>>, 20_000_000}],
+          blob_hashes_and_lengths: [{<<0::256>>, 20_000_000}]
+        )
 
       assert WorkPackage.valid?(%{wp | work_items: [medium_work_item]})
       refute WorkPackage.valid?(%{wp | work_items: [medium_work_item, medium_work_item]})
@@ -44,8 +53,16 @@ defmodule WorkPackageTest do
 
     test "validates different work_item imported_data_segments length", %{wp: wp} do
       [ds1 | rest] = Enum.map(1..500, fn _ -> {<<0::256>>, 21_062} end)
-      in_limit_work_item = build(:work_item, imported_data_segments: rest)
-      big_work_item = build(:work_item, imported_data_segments: [ds1 | rest])
+
+      in_limit_work_item =
+        build(:work_item, imported_data_segments: rest, blob_hashes_and_lengths: rest)
+
+      big_work_item =
+        build(:work_item,
+          imported_data_segments: [ds1 | rest],
+          blob_hashes_and_lengths: [ds1 | rest]
+        )
+
       # WS*WC = 4104
       # |ii| = 500 => 500 * 4104 = 2_052_000
       # Max: 12_582_912 - 2_052_000 = 10_530_912
