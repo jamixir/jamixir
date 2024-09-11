3d829458acf52b1bfcad21f6186074a8994534e9
luke <daiagi@gmail.com>
2024-09-11 16:03:07 +0700

    remove the stub

--- lib/util/crypto.ex
+++ lib/util/crypto.ex
@@ -18,15 +18,4 @@ defmodule Util.Crypto do
   def verify_signature(signature, payload, public_key) do
     :crypto.verify(:eddsa, :none, payload, signature, [public_key, :ed25519])
   end
-
-  @spec bandersnatch_ring_root(list(Types.bandersnatch_key())) :: Types.bandersnatch_ring_root()
-  def bandersnatch_ring_root(validators) do
-    # Placeholder logic: concatenate the first 4 bandersnatch keys
-    # to form a 1152-bit (144 bytes) bandersnatch ring root.
-    validators
-    # Take the first 4 validators
-    |> Enum.take(4)
-    # Concatenate them into a single binary
-    |> Enum.join()
-  end
 end
