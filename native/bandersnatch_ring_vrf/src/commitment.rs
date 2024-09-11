815cc2790d2c216ac822f2cb5763ba47a1d5d970
luke <daiagi@gmail.com>
2024-09-11 16:58:12 +0700

    skip invalid keys

--- native/bandersnatch_ring_vrf/src/commitment.rs
+++ native/bandersnatch_ring_vrf/src/commitment.rs
@@ -2,13 +2,19 @@ use ark_ec_vrfs::suites::bandersnatch::edwards::{self as bandersnatch};
 use rustler::NifResult;
 
 use crate::ring_context::ring_context;
-use crate::rustler_bridges::{FixedColumnsCommittedBridge, PublicBridge};
+use crate::rustler_bridges::public::OptionalPublicBridge;
+use crate::rustler_bridges::FixedColumnsCommittedBridge;
 
 type S = bandersnatch::BandersnatchSha512Ell2;
 
 #[rustler::nif]
-pub fn create_commitment(ring: Vec<PublicBridge<S>>) -> NifResult<FixedColumnsCommittedBridge> {
-    let pts: Vec<_> = ring.iter().map(|pk| pk.0).collect();
+pub fn create_commitment(
+    ring: Vec<OptionalPublicBridge<S>>,
+) -> NifResult<FixedColumnsCommittedBridge> {
+    let pts: Vec<_> = ring
+        .into_iter()
+        .filter_map(|OptionalPublicBridge(maybe_pk)| maybe_pk.map(|pk| pk.0))
+        .collect();
 
     let verifier_key = ring_context()?.verifier_key(&pts);
 
