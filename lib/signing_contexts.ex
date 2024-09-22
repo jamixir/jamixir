90d6b60caac255e96b88d7d602be892bdee67456
Daniel Cukier <danicuki@gmail.com>
2024-09-22 20:43:05 +0100

    cleanup signing contexts file

--- lib/signing_contexts.ex
+++ lib/signing_contexts.ex
@@ -19,12 +19,10 @@ defmodule SigningContexts do
   @doc "Bandersnatch Fallback block seal context."
   def jam_fallback_seal, do: "$jam_fallback_seal"
 
-  # Ed25519 Guarantee statements
   # Formula (142) v0.3.4
   @doc "Ed25519 Guarantee statements context."
   def jam_guarantee, do: "$jam_guarantee"
 
-  # Ed25519 Audit announcement statements
   # Formula (199) v0.3.4
   @doc "Ed25519 Audit announcement statements context."
   def jam_announce, do: "$jam_announce"
@@ -33,17 +31,14 @@ defmodule SigningContexts do
   @doc "Bandersnatch RingVRF Ticket generation and regular block seal context."
   def jam_ticket_seal, do: "$jam_ticket_seal"
 
-  # Bandersnatch Audit selection entropy
   # Formula (193) v0.3.4
   @doc "Bandersnatch Audit selection entropy context."
   def jam_audit, do: "$jam_audit"
 
-  # Ed25519 Judgements for valid work-reports
   # Formula (100) v0.3.4
   @doc "Ed25519 Judgements for valid work-reports context."
   def jam_valid, do: "$jam_valid"
 
-  # Ed25519 Judgements for invalid work-reports
   # Formula (100) v0.3.4
   @doc "Ed25519 Judgements for invalid work-reports context."
   def jam_invalid, do: "$jam_invalid"
