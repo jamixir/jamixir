7727e00baaf1dbe40f40940a469a518108719146
luke <daiagi@gmail.com>
2024-09-24 11:37:05 +0700

    fix: epoch slot sealers from json keys or tickets

--- lib/system/state/safrole.ex
+++ lib/system/state/safrole.ex
@@ -177,9 +177,16 @@ defmodule System.State.Safrole do
     %__MODULE__{
       pending: json.pending |> Enum.map(&Validator.from_json/1),
       epoch_root: Utils.hex_to_binary(json.epoch_root),
-      current_epoch_slot_sealers:
-        json.current_epoch_slot_sealers |> Enum.map(&Utils.hex_to_binary/1),
+      current_epoch_slot_sealers: parse_current_epoch_slot_sealers(json),
       ticket_accumulator: json.ticket_accumulator |> Enum.map(&SealKeyTicket.from_json/1)
     }
+  end
+
+  defp parse_current_epoch_slot_sealers(%{current_epoch_slot_sealers: %{"keys" => keys}}) do
+    keys |> Enum.map(&Utils.hex_to_binary/1)
+  end
+
+  defp parse_current_epoch_slot_sealers(%{current_epoch_slot_sealers: %{"tickets" => tickets}}) do
+    tickets |> Enum.map(&SealKeyTicket.from_json/1)
   end
 end
