85805cd0f0988c2eb9abddcc2594502435c55522
luke <daiagi@gmail.com>
2024-09-28 19:38:51 +0700

    persistant (in memory) header storage

--- lib/storage.ex
+++ lib/storage.ex
@@ -1,17 +1,106 @@
 defmodule Storage do
-  def new do
-    %{}
+  alias Block.Header
+  alias Codec.Encoder
+  alias Util.Hash
+
+  require Logger
+  use SelectiveMock
+
+  @table_name :header_store
+  def table_name, do: @table_name
+
+  def start_link do
+    init_mnesia()
   end
 
-  def put(storage, key, value) do
-    Map.put(storage, key, value)
+  def get_parent(%Header{parent_hash: parent_hash, timeslot: timeslot}) do
+    case :mnesia.dirty_read(@table_name, {timeslot - 1, parent_hash}) do
+      [{@table_name, {parent_timeslot, ^parent_hash}, parent_header}] ->
+        if parent_timeslot < timeslot do
+          {:ok, parent_header}
+        else
+          {:error, "Invalid timeslot order"}
+        end
+
+      [] ->
+        {:error, "Parent header not found"}
+    end
   end
 
-  def get(storage, key) do
-    Map.get(storage, key)
+  def put(%Header{} = header) do
+    hash = Hash.default(Encoder.encode(header))
+
+    case :mnesia.transaction(fn ->
+           :mnesia.write({@table_name, {header.timeslot, hash}, header})
+           clean_up_old_headers()
+         end) do
+      {:atomic, _} -> {:ok, hash}
+      {:aborted, reason} -> {:error, reason}
+    end
   end
 
-  def delete(storage, key) do
-    Map.delete(storage, key)
+  def get_latest do
+    case :mnesia.transaction(fn -> :mnesia.last(@table_name) end) do
+      {:atomic, :"$end_of_table"} ->
+        nil
+
+      {:atomic, key} ->
+        key
+
+      {:aborted, reason} ->
+        Logger.error("Failed to get latest finalized block header: #{inspect(reason)}")
+        nil
+    end
   end
+
+  # Private functions
+
+  defp init_mnesia do
+    :mnesia.create_schema([node()])
+    :mnesia.start()
+
+    case :mnesia.create_table(@table_name,
+           attributes: [:composite_key, :header],
+           record_name: @table_name,
+           type: :ordered_set
+         ) do
+      {:atomic, :ok} ->
+        :ok
+
+      {:aborted, {:already_exists, @table_name}} ->
+        :ok
+
+      error ->
+        Logger.error("Failed to create Mnesia table: #{inspect(error)}")
+        {:error, error}
+    end
+  end
+
+  defp clean_up_old_headers do
+    count = :mnesia.table_info(@table_name, :size)
+
+    if count >= Constants.max_age() do
+      case :mnesia.first(@table_name) do
+        :"$end_of_table" ->
+          Logger.warning("No blocks found to delete")
+
+        key ->
+          :mnesia.delete({@table_name, key})
+      end
+    end
+  end
+
+  mockable exists?(hash) do
+    case :mnesia.transaction(fn ->
+           :mnesia.match_object({@table_name, {:'$1', hash}, :'$2'})
+         end) do
+      {:atomic, [{@table_name, {_, ^hash}, _}]} -> true
+      {:atomic, []} -> false
+      {:aborted, reason} ->
+        Logger.error("Failed to check hash existence: #{inspect(reason)}")
+        false
+    end
+  end
+
+  def mock(:exists?, _), do: true
 end
--- test/storage_test.exs
+++ test/storage_test.exs
@@ -1,43 +1,78 @@
+defmodule StorageConstantsMock do
+  def max_age, do: 7
+end
+
 defmodule StorageTest do
   use ExUnit.Case
+  alias Block.Header
+  alias Util.Hash
+  alias Codec.Encoder
 
-  # Existing tests
-  test "store/2 stores the value in the storage" do
-    storage = Storage.new()
-    storage2 = Storage.put(storage, :key, "value1")
-    assert Storage.get(storage2, :key) == "value1"
+  setup_all do
+    Application.put_env(:jamixir, Constants, StorageConstantsMock)
+    on_exit(fn -> Application.delete_env(:jamixir, Constants) end)
+    :ok
   end
 
-  test "store/2 overwrites the existing value in the storage" do
-    storage = Storage.new()
-    storage2 = Storage.put(storage, :key, "value1")
-    storage3 = Storage.put(storage2, :key, "value2")
-    assert Storage.get(storage3, :key) == "value2"
+  setup do
+    Storage.start_link()
+    :mnesia.clear_table(Storage.table_name())
+    :ok
   end
 
-  # New tests
+  test "put stores and overwrites values" do
+    header1 = %Header{timeslot: 1, parent_hash: <<0::256>>}
+    header2 = %Header{timeslot: 2, parent_hash: <<0::256>>}
 
-  test "store/2 returns the updated storage" do
-    storage = Storage.new()
-    updated_storage = Storage.put(storage, :key, "value")
-    assert updated_storage == %{key: "value"}
+    assert {:ok, hash1} = Storage.put(header1)
+    assert [{_, {1, ^hash1}, ^header1}] = :mnesia.dirty_read(Storage.table_name(), {1, hash1})
+
+    assert {:ok, hash2} = Storage.put(header2)
+    assert [{_, {2, ^hash2}, ^header2}] = :mnesia.dirty_read(Storage.table_name(), {2, hash2})
+
+    assert hash1 == Hash.default(Encoder.encode(header1))
+    assert hash2 == Hash.default(Encoder.encode(header2))
   end
 
-  test "get/2 returns nil if the key does not exist in the storage" do
-    storage = Storage.new()
-    assert Storage.get(storage, :non_existing_key) == nil
+  test "get_parent returns parent or error" do
+    {:ok, hash1} = Storage.put( %Header{timeslot: 1, parent_hash: <<0::256>>})
+
+    assert {:ok, %Header{timeslot: 1}} = Storage.get_parent(%Header{timeslot: 2, parent_hash: hash1})
+    assert {:error, "Parent header not found"} = Storage.get_parent(%Header{timeslot: 3, parent_hash: "non_existing"})
   end
 
-  test "get/2 returns the value associated with the key in the storage" do
-    storage = Storage.new()
-    storage2 = Storage.put(storage, :key, "value")
-    assert Storage.get(storage2, :key) == "value"
+  test "get_latest returns latest key or nil" do
+    assert is_nil(Storage.get_latest())
+
+    header1 = %Header{timeslot: 1, parent_hash: <<0::256>>}
+    header2 = %Header{timeslot: 2, parent_hash: <<0::256>>}
+
+    {:ok, _hash1} = Storage.put(header1)
+    {:ok, hash2} = Storage.put(header2)
+
+    assert {2, ^hash2} = Storage.get_latest()
   end
 
-  test "delete/2 removes the key and its associated value from the storage" do
-    storage = Storage.new()
-    storage2 = Storage.put(storage, :key, "value")
-    storage3 = Storage.delete(storage2, :key)
-    assert Storage.get(storage3, :key) == nil
+  test "clean_up_old_headers removes oldest headers" do
+    Enum.each(1..Constants.max_age(), fn i ->
+      header = %Header{timeslot: i, parent_hash: <<i::256>>}
+      {:ok, _} = Storage.put(header)
+    end)
+
+    extra_header = %Header{timeslot: Constants.max_age() + 1, parent_hash: <<(Constants.max_age() + 1)::256>>}
+    {:ok, extra_hash} = Storage.put(extra_header)
+
+    assert :mnesia.table_info(Storage.table_name(), :size) == Constants.max_age()
+    assert :mnesia.dirty_read(Storage.table_name(), {1, :_}) == []
+    assert :mnesia.dirty_read(Storage.table_name(), {Constants.max_age() + 1, extra_hash}) != []
+    assert Storage.get_latest() == {Constants.max_age() + 1, extra_hash}
+  end
+
+  test "exists? returns true for existing hash and false for non-existing" do
+    header = %Header{timeslot: 1, parent_hash: <<0::256>>}
+    {:ok, hash} = Storage.put(header)
+
+    assert Storage.exists?(hash) == true
+    assert Storage.exists?("non_existing_hash") == false
   end
 end
