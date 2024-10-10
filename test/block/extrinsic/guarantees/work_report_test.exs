a1ce43913ce70afef702c2cc0e299ba07e1faadc
luke <daiagi@gmail.com>
2024-10-10 12:56:06 +0700

    valid_size? tests

--- test/block/extrinsic/guarantees/work_report_test.exs
+++ test/block/extrinsic/guarantees/work_report_test.exs
@@ -1,6 +1,7 @@
 defmodule WorkReportTest do
   use ExUnit.Case
   import Jamixir.Factory
+  alias Block.Extrinsic.Guarantee.WorkReport
 
   setup do
     {:ok,
@@ -13,5 +14,22 @@ defmodule WorkReportTest do
   test "encode/1 smoke test", %{wr: wr} do
     assert Codec.Encoder.encode(wr) ==
              "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x02\x01\x01\x03\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x01\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x02\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x03\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x04\x05\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x01\x02\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x03\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x04\x02\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x01\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x02\x03\0\0\0\0\0\0\0\0\x01\x04\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x01\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x02\x03\0\0\0\0\0\0\0\0\x01\x04"
+  end
+
+  describe "valid_size?/1" do
+    test "returns true for a valid work report", %{wr: wr} do
+      assert WorkReport.valid_size?(wr)
+    end
+
+    test "returns false when segment_root_lookup has more than 8 entries" do
+      invalid_wr = build(:work_report, segment_root_lookup: for(i <- 1..9, into: %{}, do: {<<i::256>>, <<i::256>>}))
+      refute WorkReport.valid_size?(invalid_wr)
+    end
+
+    test "returns false when encoded size exceeds max_work_report_size" do
+      large_output = String.duplicate("a", Constants.max_work_report_size())
+      invalid_wr = build(:work_report, output: large_output)
+      refute WorkReport.valid_size?(invalid_wr)
+    end
   end
 end
