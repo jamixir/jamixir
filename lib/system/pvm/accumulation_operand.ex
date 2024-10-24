86b45a3769263aeb1fb43efcfb1daadb9f3977b3
Daniel Cukier <danicuki@gmail.com>
2024-10-24 09:09:00 +0100

    Formula number for Operand

--- lib/system/pvm/accumulation_operand.ex
+++ lib/system/pvm/accumulation_operand.ex
@@ -2,6 +2,7 @@ defmodule System.PVM.AccumulationOperand do
   alias Block.Extrinsic.Guarantee.WorkExecutionError
   alias Types
 
+  # Formula (174) v0.4.1
   @type t :: %__MODULE__{
           o: binary() | WorkExecutionError.t(),
           l: Types.hash(),
