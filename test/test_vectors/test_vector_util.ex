422babf447353adb1abb2480dd55c5d8f01b379f
luke <daiagi@gmail.com>
2024-10-29 16:43:46 +0700

    more flexible state from json construction

--- test/test_vectors/test_vector_util.ex
+++ test/test_vectors/test_vector_util.ex
@@ -28,8 +28,8 @@ defmodule TestVectorUtil do
   def format_error("none"), do: "#{@blue}'none'#{@reset}"
   def format_error(error), do: "#{@yellow}'#{error}'#{@reset}"
 
-  def fetch_and_parse_json(file_name, path) do
-    case fetch_file(file_name, path) do
+  def fetch_and_parse_json(file_name, path, owner \\ @owner, repo \\ @repo, branch \\ @branch) do
+    case fetch_file(file_name, path, owner, repo, branch) do
       {:ok, body} -> {:ok, Jason.decode!(body) |> Utils.atomize_keys()}
       e -> e
     end
@@ -42,8 +42,8 @@ defmodule TestVectorUtil do
     end
   end
 
-  def fetch_file(file_name, path) do
-    url = "https://raw.githubusercontent.com/#{@owner}/#{@repo}/#{@branch}/#{path}/#{file_name}"
+  def fetch_file(file_name, path, owner \\ @owner, repo \\ @repo, branch \\ @branch) do
+    url = "https://raw.githubusercontent.com/#{owner}/#{repo}/#{branch}/#{path}/#{file_name}"
 
     headers = [{"User-Agent", "Elixir"}]
 
@@ -63,25 +63,24 @@ defmodule TestVectorUtil do
 
   def fetch_and_parse_local_json(file_name, path) do
     file_path = Path.join(["test", "test_vectors", path, file_name])
+
     case File.read(file_path) do
       {:ok, contents} -> {:ok, Jason.decode!(contents) |> Utils.atomize_keys()}
       error -> error
     end
   end
 
-  def assert_expected_results(json_data, tested_keys, file_name) do
+  def assert_expected_results(json_data, tested_keys, file_name, extrinsic \\ nil, header \\ nil) do
     pre_state = System.State.from_json(json_data[:pre_state])
     ok_output = json_data[:output][:ok]
 
-    extrinsic =
-      Map.from_struct(%Extrinsic{})
-      |> Map.put(:tickets, json_data[:input][:extrinsic])
-      |> Map.put(:disputes, Map.from_struct(%Disputes{}))
+    header =
+      header || Map.merge(if(ok_output == nil, do: %{}, else: ok_output), json_data[:input])
 
     block =
       Block.from_json(%{
-        extrinsic: extrinsic,
-        header: Map.merge(if(ok_output == nil, do: %{}, else: ok_output), json_data[:input])
+        extrinsic: extrinsic || default_build_extrinsic(json_data),
+        header: header
       })
 
     expected_state = System.State.from_json(json_data[:post_state])
@@ -114,5 +113,11 @@ defmodule TestVectorUtil do
     end
 
     :ok
+  end
+
+  defp default_build_extrinsic(json_data) do
+    Map.from_struct(%Extrinsic{})
+    |> Map.put(:tickets, json_data[:input][:extrinsic])
+    |> Map.put(:disputes, Map.from_struct(%Disputes{}))
   end
 end
