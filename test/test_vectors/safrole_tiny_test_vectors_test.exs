7b0a635a08ddcdea563a3f815866f65dd8577ecc
Daniel Cukier <danicuki@gmail.com>
2024-10-30 10:32:12 +0000

    refactor tests code

--- test/test_vectors/safrole_tiny_test_vectors_test.exs
+++ test/test_vectors/safrole_tiny_test_vectors_test.exs
@@ -3,6 +3,21 @@ defmodule ConstantsMock do
   def ticket_submission_end, do: 10
 end
 
+defmodule SafroleTests do
+  def files,
+    do:
+      [
+        for(i <- 1..4, do: "enact-epoch-change-with-no-tickets-#{i}"),
+        for(i <- 1..9, do: "publish-tickets-no-mark-#{i}"),
+        for(i <- 1..5, do: "publish-tickets-with-mark-#{i}"),
+        "skip-epoch-tail-1",
+        "skip-epochs-1"
+      ]
+      |> List.flatten()
+
+  def tested_keys, do: [:timeslot, :entropy_pool, :prev_validators, :curr_validators, :safrole]
+end
+
 defmodule SafroleTinyTestVectors do
   use ExUnit.Case, async: false
   import Mox
@@ -36,49 +51,19 @@ defmodule SafroleTinyTestVectors do
       assert Constants.epoch_length() == 12
     end
 
-    files_to_test = [
-      "enact-epoch-change-with-no-tickets-1",
-      "enact-epoch-change-with-no-tickets-2",
-      "enact-epoch-change-with-no-tickets-3",
-      "enact-epoch-change-with-no-tickets-4",
-      "publish-tickets-no-mark-1",
-      "publish-tickets-no-mark-2",
-      "publish-tickets-no-mark-3",
-      "publish-tickets-no-mark-4",
-      "publish-tickets-no-mark-5",
-      "publish-tickets-no-mark-6",
-      "publish-tickets-no-mark-7",
-      "publish-tickets-no-mark-8",
-      "publish-tickets-no-mark-9",
-      "publish-tickets-with-mark-1",
-      "publish-tickets-with-mark-2",
-      "publish-tickets-with-mark-3",
-      "publish-tickets-with-mark-4",
-      "publish-tickets-with-mark-5",
-      "skip-epoch-tail-1",
-      "skip-epochs-1"
-    ]
-
-    Enum.each(files_to_test, fn file_name ->
+    Enum.each(SafroleTests.files(), fn file_name ->
       @tag file_name: file_name
       @tag :tiny_test_vectors
       test "verify tiny test vectors #{file_name}", %{file_name: file_name} do
         {:ok, json_data} = fetch_and_parse_json(file_name <> ".json", @path)
 
-        HeaderSealMock
-        |> stub(:do_validate_header_seals, fn _, _, _, _ ->
+        stub(HeaderSealMock, :do_validate_header_seals, fn _, _, _, _ ->
           {:ok, %{vrf_signature_output: json_data[:input][:entropy] |> JsonDecoder.from_json()}}
         end)
 
         assert_expected_results(
           json_data,
-          [
-            :timeslot,
-            :entropy_pool,
-            :prev_validators,
-            :curr_validators,
-            :safrole
-          ],
+          SafroleTests.tested_keys(),
           file_name
         )
       end
