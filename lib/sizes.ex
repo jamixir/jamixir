0a9f1f7002343da52605c9400b271407251d757e
Daniel Cukier <danicuki@gmail.com>
2024-10-31 14:18:22 +0000

    refactor types

--- lib/sizes.ex
+++ lib/sizes.ex
@@ -4,13 +4,17 @@ defmodule Sizes do
 
   def merkle_root, do: 64
   def merkle_root_bits, do: 512
+  def signature, do: 64
+
+  def bandersnatch_signature, do: 96
+  def bandersnatch_proof, do: 784
 
   defmacro __using__(_) do
     quote do
-      @bandersnatch_proof_size 784
+      @bandersnatch_proof_size Sizes.bandersnatch_proof()
       @bitfield_size Sizes.bitfield()
       @hash_size Sizes.hash()
-      @signature_size 64
+      @signature_size Sizes.signature()
       @validator_index_size 2
     end
   end
