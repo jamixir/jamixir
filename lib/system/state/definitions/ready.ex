645c369f8dac54b824646b87ed87de16693431f8
luke <daiagi@gmail.com>
2024-11-02 17:50:57 +0700

    formula 150 v0.4.5

--- lib/system/state/definitions/ready.ex
+++ lib/system/state/definitions/ready.ex
@@ -15,4 +15,13 @@ defmodule System.State.Ready do
 
   @spec initial_state() :: list(t())
   def initial_state, do: List.duplicate([], Constants.epoch_length())
+
+  # Formula (150) v0.4.5
+  @spec q(list(list(t()))) :: MapSet.t(Types.hash())
+  def q(ready_to_accumulate) do
+    for ready <- List.flatten(ready_to_accumulate),
+        p = ready.work_report.refinement_context.prerequisite,
+        into: MapSet.new(),
+        do: p
+  end
 end
