1e67851f99a712a7e0f8da612b02717a8cc4154c
Daniel Cukier <danicuki@gmail.com>
2024-11-22 10:10:47 +0000

    passing many test vector tests

--- test/test_vectors/reports/reports_test_vectors.ex
+++ test/test_vectors/reports/reports_test_vectors.ex
@@ -7,35 +7,35 @@ defmodule ReportsTestVectors do
   def files_to_test,
     do:
       [
-        "anchor_not_recent-1"
-        # "bad_beefy_mmr-1",
-        # "bad_code_hash-1",
-        # "bad_core_index-1",
-        # "bad_service_id-1",
-        # "bad_signature-1",
-        # "bad_state_root-1",
-        # "bad_validator_index-1",
+        "anchor_not_recent-1",
+        "bad_beefy_mmr-1",
+        "bad_code_hash-1",
+        "bad_core_index-1",
+        "bad_service_id-1",
+        "bad_signature-1",
+        "bad_state_root-1",
+        "bad_validator_index-1",
         # "consume_authorization_once-1",
-        # "core_engaged-1",
-        # "dependency_missing-1",
-        # "duplicate_package_in_recent_history-1",
-        # "duplicated_package_in_report-1",
-        # "future_report_slot-1",
-        # "high_work_report_gas-1",
-        # "many_dependencies-1",
-        # "multiple_reports-1",
-        # "no_enough_guarantees-1",
-        # for(i <- 1..2, do: "not_authorized-#{i}"),
-        # "not_sorted_guarantor-1",
-        # "out_of_order_guarantees-1",
-        # "report_before_last_rotation-1",
+        "core_engaged-1",
+        "dependency_missing-1",
+        "duplicate_package_in_recent_history-1",
+        "duplicated_package_in_report-1",
+        "future_report_slot-1",
+        # "high_work_report_gas-1"
+        # "many_dependencies-1"
+        # "multiple_reports-1"
+        "no_enough_guarantees-1",
+        for(i <- 1..2, do: "not_authorized-#{i}"),
+        "not_sorted_guarantor-1",
+        "out_of_order_guarantees-1",
+        "report_before_last_rotation-1",
         # "report_curr_rotation-1",
         # "report_prev_rotation-1",
         # for(i <- 1..6, do: "reports_with_dependencies-#{i}"),
-        # for(i <- 1..2, do: "segment_root_lookup_invalid-#{i}"),
-        # "too_high_work_report_gas-1",
-        # "too_many_dependencies-1",
-        # "wrong_assignment-1"
+        for(i <- 1..2, do: "segment_root_lookup_invalid-#{i}"),
+        "too_high_work_report_gas-1",
+        "too_many_dependencies-1",
+        "wrong_assignment-1"
       ]
       |> List.flatten()
 
@@ -52,7 +52,7 @@ defmodule ReportsTestVectors do
   def setup_all do
     RingVrf.init_ring_context(Constants.validator_count())
     Application.put_env(:jamixir, :header_seal, HeaderSealMock)
-    Application.put_env(:jamixir, :original_modules, [])
+    Application.put_env(:jamixir, :original_modules, [:validate_anchor_block])
 
     on_exit(fn ->
       Application.put_env(:jamixir, :header_seal, System.HeaderSeal)
@@ -64,7 +64,13 @@ defmodule ReportsTestVectors do
 
   def execute_test(file_name, path) do
     {:ok, json_data} =
-      fetch_and_parse_json(file_name <> ".json", path, "davxy", "jam-test-vectors", "disputes")
+      fetch_and_parse_json(
+        file_name <> ".json",
+        path,
+        "davxy",
+        "jam-test-vectors",
+        "work-reports"
+      )
 
     extrinsic =
       Map.from_struct(%Extrinsic{})
@@ -75,6 +81,8 @@ defmodule ReportsTestVectors do
 
     header =
       Map.merge(if(ok_output == nil, do: %{}, else: ok_output), json_data[:input])
+
+    json_data = put_in(json_data[:pre_state][:eta], json_data[:input][:entropy])
 
     assert_expected_results(json_data, tested_keys(), file_name, extrinsic, header)
   end
