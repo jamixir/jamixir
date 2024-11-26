29ddf5aedf96b81fe6502950d6f7dd4b04468cde
luke <daiagi@gmail.com>
2024-11-26 17:15:59 +0700

    nicer constants

--- lib/pvm/constants.ex
+++ lib/pvm/constants.ex
@@ -2,16 +2,16 @@ defmodule PVM.Constants do
   # Apendix B.1
   defmacro __using__(_) do
     quote do
-      @none 4_294_967_296 - 1
-      @what 4_294_967_296 - 2
-      @oob 4_294_967_296 - 3
-      @who 4_294_967_296 - 4
-      @full 4_294_967_296 - 5
-      @core 4_294_967_296 - 6
-      @cash 4_294_967_296 - 7
-      @low 4_294_967_296 - 8
-      @high 4_294_967_296 - 9
-      @huh 4_294_967_296 - 10
+      @none 0xFFFFFFFFFFFFFFFF
+      @what 0xFFFFFFFFFFFFFFFE
+      @oob 0xFFFFFFFFFFFFFFFD
+      @who 0xFFFFFFFFFFFFFFFC
+      @full 0xFFFFFFFFFFFFFFFB
+      @core 0xFFFFFFFFFFFFFFFA
+      @cash 0xFFFFFFFFFFFFFFF9
+      @low 0xFFFFFFFFFFFFFFF8
+      @high 0xFFFFFFFFFFFFFFF7
+      @huh 0xFFFFFFFFFFFFFFF6
       @ok 0
 
       @gas 0
