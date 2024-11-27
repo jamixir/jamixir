fd04fe48a34e1d7b5c3906934c5bf8e662e6de0c
luke <daiagi@gmail.com>
2024-11-27 16:47:22 +0700

    refactor pvm constants

---
+++ lib/pvm/constants/host_call_ids.ex
@@ -0,0 +1,5 @@
+defmodule PVM.Constants.HostCallId do
+  @gas 0
+
+  def gas, do: @gas
+end
---
+++ lib/pvm/constants/host_call_result.ex
@@ -0,0 +1,26 @@
+defmodule PVM.Constants.HostCallResult do
+  # Apendix B.1
+  @none 0xFFFFFFFFFFFFFFFF
+  @what 0xFFFFFFFFFFFFFFFE
+  @oob 0xFFFFFFFFFFFFFFFD
+  @who 0xFFFFFFFFFFFFFFFC
+  @full 0xFFFFFFFFFFFFFFFB
+  @core 0xFFFFFFFFFFFFFFFA
+  @cash 0xFFFFFFFFFFFFFFF9
+  @low 0xFFFFFFFFFFFFFFF8
+  @high 0xFFFFFFFFFFFFFFF7
+  @huh 0xFFFFFFFFFFFFFFF6
+  @ok 0
+
+  def none, do: @none
+  def what, do: @what
+  def oob, do: @oob
+  def who, do: @who
+  def full, do: @full
+  def core, do: @core
+  def cash, do: @cash
+  def low, do: @low
+  def high, do: @high
+  def huh, do: @huh
+  def ok, do: @ok
+end
