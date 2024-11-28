e594c3e5c7149511e71e24ef16935e6d8ac0e278
Daniel Cukier <danicuki@gmail.com>
2024-11-28 10:26:07 +0000

    skip only failing safrole vectors

--- test/test_vectors/safrole/safrole_tiny_test_vectors_test.exs
+++ test/test_vectors/safrole/safrole_tiny_test_vectors_test.exs
@@ -11,7 +11,19 @@ defmodule SafroleTinyTestVectors do
   describe "vectors" do
     Enum.each(files(), fn file_name ->
       @tag file_name: file_name
-      @tag :skip
+      # skip tests that are failing because of wrong test vectors for 0.5
+      @tag if(
+             file_name in [
+               "enact-epoch-change-with-no-tickets-4",
+               "skip-epochs-1",
+               "skip-epoch-tail-1",
+               "publish-tickets-with-mark-5",
+               "publish-tickets-no-mark-1",
+               "publish-tickets-no-mark-9"
+             ],
+             do: :skip
+           )
+
       test "verify tiny test vectors #{file_name}", %{file_name: file_name} do
         execute_test(file_name, "safrole/tiny")
       end
