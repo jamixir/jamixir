b13c746170bc0562c73fbe0664d450c00062ce78
luke <daiagi@gmail.com>
2024-11-28 17:36:38 +0700

    sync with other names

--- lib/block/extrinsic/guarantees/work_execution_error.ex
+++ lib/block/extrinsic/guarantees/work_execution_error.ex
@@ -1,10 +1,10 @@
 defmodule Block.Extrinsic.Guarantee.WorkExecutionError do
   alias Util.Collections
   # Formula (123) v0.4.5
-  @type t :: :infinite | :halt | :bad | :big
+  @type t :: :out_of_gas | :panic | :bad | :big
 
   # Formula (C.28) v0.5.0
-  @codes %{infinite: 1, halt: 2, bad: 3, big: 4}
+  @codes %{out_of_gas: 1, panic: 2, bad: 3, big: 4}
 
   def code(error) do
     @codes[error]
