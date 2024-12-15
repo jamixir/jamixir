f9255021a50f31c89813ddc6ea7e70ac81333395
luke <daiagi@gmail.com>
2024-12-15 21:12:39 +0700

    accumulate utils

--- lib/pvm/accumulate/utils.ex
+++ lib/pvm/accumulate/utils.ex
@@ -1,9 +1,12 @@
 defmodule PVM.Accumulate.Utils do
+  alias System.State.ServiceAccount
+  alias System.DeferredTransfer
   alias System.State.Accumulation
   alias PVM.Accumulate.Context
   alias Util.Hash
   use Codec.{Encoder, Decoder}
 
+  @hash_size 32
 
   # Formula (B.9) v0.5.2
   @spec initializer(Accumulation.t(), non_neg_integer(), Types.hash(), non_neg_integer()) ::
@@ -40,5 +43,26 @@ defmodule PVM.Accumulate.Utils do
       new_i = rem(i - 0x100 + 1, 0xFFFFFE00) + 0x100
       check(new_i, keys)
     end
+  end
+
+  # Formula (B.12) v0.5.2
+  @spec collapse({non_neg_integer(), binary() | :panic | :out_of_gas, {Context.t(), Context.t()}}) ::
+          {Accumulation.t(), list(DeferredTransfer.t()), Types.hash() | nil, non_neg_integer()}
+  def collapse({gas, output, {x, _y}}) when is_binary(output) and byte_size(output) == @hash_size,
+    do: {x.accumulation, x.transfers, output, gas}
+
+  def collapse({gas, output, {x, _y}}) when is_binary(output),
+    do: {x.accumulation, x.transfers, nil, gas}
+
+  def collapse({gas, output, {_x, y}}) when output in [:panic, :out_of_gas],
+    do: {y.accumulation, y.transfers, nil, gas}
+
+  @spec replace_service(
+          {PVM.Types.exit_reason(), PVM.Types.host_call_state(), ServiceAccount.t()},
+          {Context.t(), Context.t()}
+        ) :: {PVM.Types.exit_reason(), PVM.Types.host_call_state(), ServiceAccount.t()}
+  def replace_service({exit_reason, state, service_account}, {x, y}) do
+    new_x = put_in(x, [:accumulation, :services, x.service], service_account)
+    {exit_reason, state, {new_x, y}}
   end
 end
