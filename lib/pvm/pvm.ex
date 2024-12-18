4d5e4b385de501d789d7c1bcd0eb0edc3b674507
luke <daiagi@gmail.com>
2024-12-18 12:27:16 +0700

    sync authorized to new result struct

--- lib/pvm/pvm.ex
+++ lib/pvm/pvm.ex
@@ -1,5 +1,4 @@
 defmodule PVM do
-
   alias System.State.ServiceAccount
   alias PVM.{ArgInvoc, Host, Registers}
   alias Block.Extrinsic.{Guarantee.WorkExecutionError, WorkPackage}
@@ -7,16 +6,8 @@ defmodule PVM do
   import PVM.Constants.{HostCallId, HostCallResult}
   import PVM.Host.Gas
 
-  @doc """
-    Ψ1: The single-step (pvm) machine state-transition function.
-    ΨA: The Accumulate pvm invocation function.
-    ΨH : The host-function invocation (pvm) with host-function marshalling.
-    ΨT : The On-Transfer pvm invocation function.
-    Ω: Virtual machine host-call functions.
-  """
-
   # ΨI : The Is-Authorized pvm invocation function.
-  # Formula (273) v0.4.5
+  # Formula (B.1) v0.5.2
   @spec authorized(WorkPackage.t(), non_neg_integer(), %{integer() => ServiceAccount.t()}) ::
           binary() | WorkExecutionError.t()
   def authorized(p = %WorkPackage{}, core, services) do
@@ -28,12 +19,14 @@ defmodule PVM do
     r
   end
 
-  # Formula (274) v0.4.5
+  # Formula (B.2) v0.5.2
   @spec authorized_f(non_neg_integer(), PVM.Types.host_call_state(), PVM.Types.context()) ::
           {PVM.Types.exit_reason(), PVM.Types.host_call_state(), PVM.Types.context()}
   def authorized_f(n, %{gas: gas, registers: registers, memory: memory}, _context) do
     if host(n) == :gas do
-      {exit_reason, gas_, registers_, _} = Host.General.gas(gas, registers, memory, nil)
+      %{exit_reason: exit_reason, gas: gas_, registers: registers_, memory: memory} =
+        Host.General.gas(gas, registers, memory, nil)
+
       {exit_reason, {gas_, registers_, memory}, nil}
     else
       {:continue,
