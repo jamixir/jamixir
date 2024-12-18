35234aa71ee22e05b10f0803a4e837913dadb325
luke <daiagi@gmail.com>
2024-12-18 13:19:34 +0700

    on transfer invocation

--- lib/pvm/pvm.ex
+++ lib/pvm/pvm.ex
@@ -1,4 +1,6 @@
 defmodule PVM do
+  alias Util.Hash
+  alias PVM.Host.General
   alias System.DeferredTransfer
   alias System.State.{Accumulation, ServiceAccount}
   alias PVM.{Accumulate.Operand, ArgInvoc, Host, Registers, Refine.Params, Host.Accumulate}
@@ -59,5 +61,69 @@ defmodule PVM do
         }
   def accumulate(accumulation_state, timeslot, service_index, gas, operands, init_fn) do
     PVM.Accumulate.execute(accumulation_state, timeslot, service_index, gas, operands, init_fn)
+  end
+
+  # Formula (B.14) v0.5.2
+  @spec on_transfer(
+          services :: %{integer() => ServiceAccount.t()},
+          timeslot :: non_neg_integer(),
+          service_index :: non_neg_integer(),
+          transfers :: list(DeferredTransfer.t())
+        ) :: ServiceAccount.t()
+  def on_transfer(services, timeslot, service_index, transfers) do
+
+    # Formula (B.16) v0.5.2
+    f = fn n, %{gas: gas, registers: registers, memory: memory}, context ->
+      host_call_result =
+        case host(n) do
+          :lookup ->
+            General.lookup(gas, registers, memory, context, service_index, services)
+
+          :read ->
+            General.read(gas, registers, memory, context, service_index, services)
+
+          :write ->
+            General.write(gas, registers, memory, context, service_index)
+
+          :gas ->
+            General.gas(gas, registers, memory, context)
+
+          :info ->
+            General.info(gas, registers, memory, context, service_index, services)
+
+          _ ->
+            %{
+              exit_reason: :continue,
+              gas: gas - default_gas(),
+              registers: registers,
+              memory: memory,
+              context: context
+            }
+        end
+
+      %{exit_reason: e, gas: g, registers: r, memory: m, context: c} = host_call_result
+
+      {e, %{gas: g, registers: r, memory: m}, c}
+    end
+
+    # Formula (B.15) v0.5.2
+    service =
+      Map.get(services, service_index)
+      |> update_in(
+        [:balance],
+        &for t <- transfers, reduce: &1 do
+          acc -> acc + t.amount
+        end
+      )
+
+    sc = Map.get(service, :code_hash)
+
+    if sc == nil or sc == Hash.zero() do
+      service
+    else
+      gas_limit = Enum.sum(Enum.map(transfers, & &1.gas_limit))
+      {_, _, service_} = ArgInvoc.execute(sc, 10, gas_limit, e({timeslot, service_index, vs(transfers)}), f, service)
+      service_
+    end
   end
 end
