dda8635eca4afb98062b8f973438adcd5a6d6862
luke <daiagi@gmail.com>
2024-12-18 12:37:52 +0700

    expose invocation at top level

--- lib/pvm/pvm.ex
+++ lib/pvm/pvm.ex
@@ -1,6 +1,7 @@
 defmodule PVM do
-  alias System.State.ServiceAccount
-  alias PVM.{ArgInvoc, Host, Registers}
+  alias System.DeferredTransfer
+  alias System.State.{Accumulation, ServiceAccount}
+  alias PVM.{Accumulate.Operand, ArgInvoc, Host, Registers, Refine.Params, Host.Accumulate}
   alias Block.Extrinsic.{Guarantee.WorkExecutionError, WorkPackage}
   use Codec.{Encoder, Decoder}
   import PVM.Constants.{HostCallId, HostCallResult}
@@ -36,5 +37,27 @@ defmodule PVM do
          memory: memory
        }, nil}
     end
+  end
+
+  # Formula (B.8) v0.5.2
+  @spec refine(Params.t(), %{integer() => ServiceAccount.t()}) ::
+          {binary() | WorkExecutionError.t(), list(binary())}
+  def refine(%Params{} = params, services), do: PVM.Refine.execute(params, services)
+
+  @spec accumulate(
+          accumulation_state :: Accumulation.t(),
+          timeslot :: non_neg_integer(),
+          service_index :: non_neg_integer(),
+          gas :: non_neg_integer(),
+          operands :: list(Operand.t()),
+          init_fn :: (Accumulation.t(), non_neg_integer() -> Accumulate.Context.t())
+        ) :: {
+          Accumulation.t(),
+          list(DeferredTransfer.t()),
+          Types.hash() | nil,
+          non_neg_integer()
+        }
+  def accumulate(accumulation_state, timeslot, service_index, gas, operands, init_fn) do
+    PVM.Accumulate.execute(accumulation_state, timeslot, service_index, gas, operands, init_fn)
   end
 end
