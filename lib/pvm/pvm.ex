3d23e48fc2acffe22ca9832c76307561aaa4df50
luke <daiagi@gmail.com>
2024-12-20 15:20:34 +0700

    comment about departure from GP

--- lib/pvm/pvm.ex
+++ lib/pvm/pvm.ex
@@ -122,6 +122,7 @@ defmodule PVM do
     else
       gas_limit = Enum.sum(Enum.map(transfers, & &1.gas_limit))
 
+      #https://github.com/gavofyork/graypaper/pull/170
       {_, _, service_} =
         ArgInvoc.execute(
           code,
