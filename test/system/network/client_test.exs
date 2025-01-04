af7152541996eca19085af76ee23b5e770c4ab8c
Daniel Cukier <danicuki@gmail.com>
2025-01-04 09:22:28 +0000

    added node api so we can mock it on network tests (#288)

--- test/system/network/client_test.exs
+++ test/system/network/client_test.exs
@@ -1,9 +1,12 @@
 defmodule System.Network.ClientTest do
   use ExUnit.Case, async: false
-
+  import Mox
   alias System.Network.{Client, Server}
 
   @port 9999
+
+  # needs this because server uses mock in another process
+  setup :set_mox_from_context
 
   setup_all do
     # Start server in supervised process
@@ -23,21 +26,28 @@ defmodule System.Network.ClientTest do
       end
     end)
 
+    blocks = for _ <- 1..3, {b, _} = Block.decode(File.read!("test/block_mock.bin")), do: b
+
     # Return server context to tests
-    {:ok, server: server_pid}
+    {:ok, server: server_pid, blocks: blocks}
   end
 
   describe "ask_block/3" do
     @describetag :sequential
 
-    test "asks 10 blocks" do
-      blocks = Client.ask_block(<<0::32>>, 0, 9)
-      assert length(blocks) == 9
+    test "asks 9 blocks", %{blocks: blocks} do
+      Jamixir.NodeAPI.Mock |> expect(:get_blocks, fn 0, 0, 9 -> {:ok, blocks} end)
+      result = Client.ask_block(<<0::32>>, 0, 9)
+      verify!()
+
+      assert result == blocks
     end
 
-    test "asks 2 blocks" do
-      blocks = Client.ask_block(<<0::32>>, 0, 2)
-      assert length(blocks) == 2
+    test "asks 2 in desc order blocks", %{blocks: blocks} do
+      Jamixir.NodeAPI.Mock |> expect(:get_blocks, fn 1, 1, 2 -> {:ok, blocks} end)
+      result = Client.ask_block(<<1::32>>, 1, 2)
+      verify!()
+      assert result == blocks
     end
   end
 end
