8c1060e144ad2eda9539288ad1b6e47fe6e3ee44
luke <daiagi@gmail.com>
2025-01-09 19:50:54 +0700

    all tests pass

--- test/quic/request_blocks_test.exs
+++ test/quic/request_blocks_test.exs
@@ -4,85 +4,43 @@ defmodule BasicQuicClientTest do
   alias Quic.{Client, Server}
   require Logger
 
-  setup do
-    # Logger.configure(
-    #   level: :debug,
-    #   sync: false
-    # )
-
-    # Add filter for QUIC client logs
-    # :logger.add_primary_filter(
-    #   :quic_client_filter,
-    #   {&QuicLogFilter.filter/2, []}
-    # )
-
-    :ok
-  end
-
   @port 9999
 
-  # needs this because server uses mock in another process
-  setup :set_mox_from_context
+  setup do
+    # Start server and client for each test
+    {:ok, server} = Server.start_link(@port)
+    {:ok, client} = Client.start_link(port: @port)
+    Process.sleep(100)
 
-  setup_all do
-    Logger.configure(
-      level: :debug,
-      sync: false
-    )
-
-    # Start server in supervised process
-    {:ok, server_pid} =
-      Task.start(fn ->
-        Server.start_link(@port)
-      end)
-
-    # Give server time to initialize
-    :timer.sleep(100)
-
-    # Cleanup callback
+    # Cleanup after each test
     on_exit(fn ->
-      if Process.alive?(server_pid) do
-        Process.exit(server_pid, :normal)
-      end
+      if Process.alive?(server), do: Process.exit(server, :normal)
+      if Process.alive?(client), do: Process.exit(client, :normal)
     end)
 
     blocks = for _ <- 1..3, {b, _} = Block.decode(File.read!("test/block_mock.bin")), do: b
-
-    # Return server context to tests
-    {:ok, server: server_pid, blocks: blocks}
+    {:ok, client: client, blocks: blocks}
   end
 
+  setup :set_mox_from_context
+
   describe "request_blocks/4" do
-    @describetag :sequential
-
-    test "requests 9 blocks", %{blocks: blocks} do
-      {:ok, client} = Client.start_link(port: @port)
-
+    test "requests 9 blocks", %{client: client, blocks: blocks} do
       Jamixir.NodeAPI.Mock
       |> expect(:get_blocks, fn 0, 0, 9 -> {:ok, blocks} end)
 
       result = Client.request_blocks(client, <<0::32>>, 0, 9)
       verify!()
-
-      assert match?({:ok, _}, result)
-      {:ok, response} = result
-
-      assert response == blocks
+      assert {:ok, ^blocks} = result
     end
 
-    test "requests 2 blocks in descending order", %{blocks: blocks} do
-      {:ok, client} = Client.start_link(port: @port)
-
+    test "requests 2 blocks in descending order", %{client: client, blocks: blocks} do
       Jamixir.NodeAPI.Mock
       |> expect(:get_blocks, fn 1, 1, 2 -> {:ok, blocks} end)
 
       result = Client.request_blocks(client, <<1::32>>, 1, 2)
       verify!()
-
-      assert match?({:ok, _}, result)
-      {:ok, response} = result
-
-      assert response == blocks
+      assert {:ok, ^blocks} = result
     end
   end
 end
