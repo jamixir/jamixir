91b5c5e7c4db4ff5d1ca31b02711cc31c137f04c
luke <daiagi@gmail.com>
2025-01-10 19:16:37 +0700

    minor code cleanups

--- lib/network/message_handler.ex
+++ lib/network/message_handler.ex
@@ -3,22 +3,26 @@ defmodule Network.MessageHandler do
   import Bitwise
   alias Quicer.Flags
 
-  def handle_stream_data(data, stream, %{flags: flags} = props, state, opts \\ []) do
+  def handle_stream_data(
+        data,
+        stream,
+        %{flags: flags} = props,
+        %{streams: streams} = state,
+        opts \\ []
+      ) do
     log_tag = Keyword.get(opts, :log_tag, "[MESSAGE_HANDLER]")
     on_complete = Keyword.get(opts, :on_complete)
 
     Logger.debug("#{log_tag} Props: #{inspect(props)}")
     Logger.debug("#{log_tag} Data: #{inspect(data)}")
 
-    # Get the binary buffer from the stream state
-    stream_state = Map.get(state.streams, stream, %{})
-    buffer = Map.get(stream_state, :buffer, <<>>)
-    new_buffer = buffer <> data
-    Logger.debug("#{log_tag} new_buffer: #{inspect(new_buffer)}")
+    stream_state = Map.get(streams, stream, %{})
+    buffer_ = Map.get(stream_state, :buffer, <<>>) <> data
+    Logger.debug("#{log_tag} buffer_: #{inspect(buffer_)}")
 
     if (flags &&& Flags.receive_flag(:fin)) != 0 do
       Logger.debug("#{log_tag} FIN flag is set")
-      <<protocol_id::8, length::32-little, message::binary-size(length)>> = new_buffer
+      <<protocol_id::8, length::32-little, message::binary-size(length)>> = buffer_
 
       case on_complete do
         nil ->
@@ -30,9 +34,8 @@ defmodule Network.MessageHandler do
       end
     else
       Logger.debug("#{log_tag} More data coming, keep buffering")
-      # Preserve other stream state fields while updating the buffer
-      new_stream_state = Map.put(stream_state, :buffer, new_buffer)
-      {:noreply, %{state | streams: Map.put(state.streams, stream, new_stream_state)}}
+      stream_state_ = Map.put(stream_state, :buffer, buffer_)
+      {:noreply, %{state | streams: Map.put(state.streams, stream, stream_state_)}}
     end
   end
 
