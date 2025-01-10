8aea0001bb6a2bafeb712604377957ffca298541
luke <daiagi@gmail.com>
2025-01-10 17:27:34 +0700

    unify tests, alter ports

--- test/quic/request_blocks_test.exs
+++ test/quic/request_blocks_test.exs
@@ -1,23 +1,51 @@
-defmodule BasicQuicClientTest do
+defmodule QuicTest do
   use ExUnit.Case, async: false
   import Mox
-  alias Quic.{Client}
   require Logger
+  alias Quic.{Client}
 
-  @port 8999
-  @server_name :quic_server_blocks_test
-  @client_name :quic_client_blocks_test
+  @base_port 9999
 
-  setup do
-    {_server_pid, client_pid} = QuicTestHelper.start_quic_processes(@port, @server_name, @client_name)
+  setup_all do
+    Logger.configure(level: :none)
+    :ok
+  end
+
+  setup context do
+    # Use a different port for each test based on its line number
+    port = @base_port + (context.line || 0)
     blocks = for _ <- 1..3, {b, _} = Block.decode(File.read!("test/block_mock.bin")), do: b
-    {:ok, client: client_pid, blocks: blocks}
+    {server_pid, client_pid} = QuicTestHelper.start_quic_processes(port)
+
+    on_exit(fn ->
+      QuicTestHelper.cleanup_processes(server_pid, client_pid)
+    end)
+
+    {:ok, client: client_pid, blocks: blocks, port: port}
   end
 
   setup :set_mox_from_context
 
+  test "parallel streams", %{client: client, port: _port} do
+    number_of_messages = 2
+
+    tasks =
+      for i <- 1..number_of_messages do
+        Task.async(fn ->
+          message = "Hello, server#{i}!"
+          {:ok, response} = Client.send(client, 127, message)
+          Logger.info("[QUIC_TEST] Response #{i}: #{inspect(response)}")
+          assert response == message
+          i
+        end)
+      end
+
+    results = Task.await_many(tasks, 5000)
+    assert length(results) == number_of_messages
+  end
+
   describe "request_blocks/4" do
-    test "requests 9 blocks", %{client: client, blocks: blocks} do
+    test "requests 9 blocks", %{client: client, blocks: blocks, port: _port} do
       Jamixir.NodeAPI.Mock
       |> expect(:get_blocks, fn 0, 0, 9 -> {:ok, blocks} end)
 
@@ -26,7 +54,7 @@ defmodule BasicQuicClientTest do
       assert {:ok, ^blocks} = result
     end
 
-    test "requests 2 blocks in descending order", %{client: client, blocks: blocks} do
+    test "requests 2 blocks in descending order", %{client: client, blocks: blocks, port: _port} do
       Jamixir.NodeAPI.Mock
       |> expect(:get_blocks, fn 1, 1, 2 -> {:ok, blocks} end)
 
--- test/support/quic_test_helper.ex
+++ test/support/quic_test_helper.ex
@@ -1,24 +1,15 @@
 defmodule QuicTestHelper do
   use ExUnit.Case
-  def start_quic_processes(port, server_name, client_name) do
+
+  def start_quic_processes(port, server_name \\ nil, client_name \\ nil) do
     {:ok, server_pid} = Quic.Server.start_link(port, name: server_name)
     {:ok, client_pid} = Quic.Client.start_link([port: port], name: client_name)
     Process.sleep(100)
-
-    on_exit(fn ->
-      try do
-        if pid = Process.whereis(server_name), do: GenServer.stop(pid)
-      catch
-        _kind, _reason -> :ok
-      end
-
-      try do
-        if pid = Process.whereis(client_name), do: GenServer.stop(pid)
-      catch
-        _kind, _reason -> :ok
-      end
-    end)
-
     {server_pid, client_pid}
+  end
+
+  def cleanup_processes(server_pid, client_pid) do
+    if Process.alive?(server_pid), do: Process.exit(server_pid, :kill)
+    if Process.alive?(client_pid), do: Process.exit(client_pid, :kill)
   end
 end
