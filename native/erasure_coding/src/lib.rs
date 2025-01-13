1c94bbc037da91fb15c02a4cb71f0ac43b861678
Daniel Cukier <danicuki@gmail.com>
2025-01-13 10:02:07 +0000

    algorithm working padding 2 bytes with 62 bytes

--- native/erasure_coding/src/lib.rs
+++ native/erasure_coding/src/lib.rs
@@ -9,20 +9,35 @@ fn encode(data: Vec<Vec<u8>>) -> NifResult<Vec<Vec<u8>>> {
 }
 
 fn do_encode(data: Vec<Vec<u8>>) -> Result<Vec<Vec<u8>>, reed_solomon::Error> {
-    let original_count = data.len();
-    println!("line 1");
-    let mut encoder = ReedSolomonEncoder::new(
-        original_count,  // total number of original shards
-        1023, // total number of recovery shards
-        684 as usize,  // shard size in bytes
-    )?;
-    println!("line 2");
+    if data.len() != 342 {
+        return Err(reed_solomon::Error::NotEnoughShards {
+            original_count: 342,
+            original_received_count: data.len(),
+            recovery_received_count: 1023,
+        });
+    }
 
-    for shard in data {
+    // TODO this is temporary until we can get the erasure coding to work with 2 bytes
+    let padded_data: Vec<Vec<u8>> = data
+        .into_iter()
+        .map(|mut v| {
+            let mut padded = vec![0u8; 64]; // Create vector with 64 zeros
+            padded[..v.len()].copy_from_slice(&v); // Copy original bytes to start
+            padded
+        })
+        .collect();
+
+    let mut encoder = ReedSolomonEncoder::new(
+        342,  // total number of original shards
+        1023, // total number of recovery shards
+        64,   // shard size in bytes
+    )?;
+
+    for shard in padded_data {
         encoder.add_original_shard(shard)?;
     }
-    println!("line 3");
 
+    // let result = reed_solomon::encode(382, 1023, data)?;
     let result = encoder.encode()?;
 
     let recovery: Vec<Vec<u8>> = result.recovery_iter().map(|s| s.to_vec()).collect();
@@ -36,9 +51,11 @@ mod tests {
 
     #[test]
     fn test_encode() {
-        let data: Vec<u8> = (1..=684).map(|x| (x % 256) as u8).collect();
-        let result = do_encode(vec![data]).unwrap();
-      assert_eq!(1, 1);
+        let data: Vec<Vec<u8>> = (1..=342)
+            .map(|x: u16| vec![(x % 255) as u8, (x % 255) as u8])
+            .collect();
+        let result = do_encode(data).unwrap();
+        assert_eq!(1, 1);
         assert_eq!(result.len(), 1023);
     }
 }
