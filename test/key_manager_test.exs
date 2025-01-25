fc18e5c1c39ef7afc52667e96c54a03377fcb95a
Daniel Cukier <danicuki@gmail.com>
2025-01-25 18:48:41 +0000

    improve key manager and block creation using env key

--- test/key_manager_test.exs
+++ test/key_manager_test.exs
@@ -1,6 +1,7 @@
 defmodule KeyManagerTest do
   alias Util.Hash
   use ExUnit.Case
+  import Util.Hex
 
   setup_all do
     {:ok, private_key_file} = Temp.path(%{suffix: ".enc"})
@@ -37,6 +38,40 @@ defmodule KeyManagerTest do
       {private_key, public_key} = KeyManager.get_keypair()
       assert private_key == KeyManager.get_private_key()
       assert public_key == KeyManager.get_public_key()
+    end
+  end
+
+  @keys_file "test/keys/0.json"
+
+  describe "load_keys/1" do
+    setup do
+      on_exit(fn ->
+        Application.delete_env(:jamixir, :keys)
+      end)
+
+      :ok
+    end
+
+    test "loads keys from valid JSON file" do
+      KeyManager.load_keys(@keys_file)
+      keys = Application.get_env(:jamixir, :keys)
+
+      assert keys.ed25519 ==
+               decode16!("0x3b6a27bcceb6a42d62a3a8d02a6f0d73653215771de243a63ac048a18b59da29")
+
+      assert keys.ed25519_priv ==
+               decode16!("0x0000000000000000000000000000000000000000000000000000000000000000")
+    end
+
+    test "handles missing file" do
+      assert {:error, :enoent} = KeyManager.load_keys("nonexistent.json")
+    end
+
+    test "handles invalid JSON" do
+      invalid_file = "test/keys/invalid.json"
+      File.write!(invalid_file, "invalid json")
+      assert {:error, %Jason.DecodeError{}} = KeyManager.load_keys(invalid_file)
+      File.rm!(invalid_file)
     end
   end
 end
