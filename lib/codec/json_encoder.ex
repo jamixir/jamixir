356dde35943a2fc54563174ef4b528be93bd188c
luke <daiagi@gmail.com>
2025-01-27 09:29:20 +0700

    recent history json codec

--- lib/codec/json_encoder.ex
+++ lib/codec/json_encoder.ex
@@ -1,10 +1,50 @@
 defmodule Codec.JsonEncoder do
   import Util.Hex
-  def to_json(struct) when is_map(struct) do
-    for x <- Map.from_struct(struct), do: encode_field(x), into: %{}
+
+  def encode(%{__struct__: module} = struct) do
+    # Get key mappings if module has them, otherwise empty map
+    key_mapping =
+      if function_exported?(module, :to_json_mapping, 0), do: module.to_json_mapping(), else: %{}
+
+    # Start with struct-to-map conversion
+    base =
+      struct
+      |> Map.from_struct()
+      # Remove keys that will be transformed
+      |> Map.drop(Map.keys(key_mapping))
+
+    # Handle root transformation separately
+    case Enum.find(key_mapping, fn {_, mapping} -> match?({:_root, _}, mapping) or mapping == :_root end) do
+      {key, {:_root, transform}} ->
+        transform.(Map.get(struct, key)) |> encode()
+
+      {key, :_root} ->
+        encode(Map.get(struct, key))
+
+      _ ->
+        # Normal field transformations
+        transformed =
+          for {old_key, mapping} <- key_mapping, into: %{} do
+            original_value = Map.get(struct, old_key)
+
+            case mapping do
+              {new_key, transform} -> {new_key, transform.(original_value)}
+              new_key when is_atom(new_key) -> {new_key, original_value}
+            end
+          end
+
+        Map.merge(base, transformed) |> Map.new(fn {k, v} -> {k, encode(v)} end)
+    end
   end
 
-  defp encode_field({key, value}) when is_binary(value) do
-    {key, encode16(value, prefix: true)}
+  def encode(value) when is_list(value), do: Enum.map(value, &encode/1)
+  def encode(value) when is_map(value), do: for({k, v} <- value, into: %{}, do: {k, encode(v)})
+  def encode(value) when is_binary(value), do: encode16(value, prefix: true)
+  def encode(value), do: value
+
+  defmacro __using__(_) do
+    quote do
+      def to_json(struct), do: JsonEncoder.encode(struct)
+    end
   end
 end
---
+++ test/codec/json_encoder_test.exs
@@ -0,0 +1,71 @@
+defmodule Codec.JsonEncoderTest do
+  use ExUnit.Case
+  alias Codec.JsonEncoder
+  alias System.State.RecentHistory.RecentBlock
+  alias Util.{Hash, Hex}
+  import Jamixir.Factory
+
+  describe "encode/1" do
+    test "encodes list of list of hashes" do
+      # Create some test binary data representing hashes
+      hash1 = <<1, 2, 3>>
+      hash2 = <<4, 5, 6>>
+      hash3 = <<7, 8, 9>>
+
+      input = [
+        [hash1, hash2],
+        [hash3]
+      ]
+
+      expected = [
+        ["0x010203", "0x040506"],
+        ["0x070809"]
+      ]
+
+      assert JsonEncoder.encode(input) == expected
+    end
+
+    test "encodes empty lists" do
+      input = [[], []]
+      expected = [[], []]
+
+      assert JsonEncoder.encode(input) == expected
+    end
+
+    test "encodes single hash" do
+      hash = <<1, 2, 3>>
+      assert JsonEncoder.encode(hash) == "0x010203"
+    end
+
+    test "encodes RecentBlock" do
+      block =
+        build(:recent_block,
+          work_report_hashes: %{Hash.random() => Hash.random(), Hash.random() => Hash.random()},
+          accumulated_result_mmr: [Hash.random(), Hash.random(), nil]
+        )
+
+      json = JsonEncoder.encode(block)
+
+      assert json == %{
+               header_hash: Hex.encode16(block.header_hash, prefix: true),
+               state_root: Hex.encode16(block.state_root, prefix: true),
+               mmr:
+                 for hash <- block.accumulated_result_mmr do
+                   if is_nil(hash), do: nil, else: Hex.encode16(hash, prefix: true)
+                 end,
+               reported:
+                 block.work_report_hashes
+                 |> Enum.map(fn {hash, exports_root} ->
+                   %{hash: Hex.encode16(hash, prefix: true), exports_root: Hex.encode16(exports_root, prefix: true)}
+                 end)
+             }
+    end
+
+
+    test "encodes RecentHistory" do
+      history = build(:recent_history)
+      json = JsonEncoder.encode(history)
+      assert json == Enum.map(history.blocks, &JsonEncoder.encode/1)
+    end
+  end
+end
