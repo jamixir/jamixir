1f6bf44696736ef159bd406ba1533d1534a1376b
luke <daiagi@gmail.com>
2025-01-29 13:23:04 +0700

    small cleanup

--- lib/codec/scale_native.ex
+++
@@ -1,6 +0,0 @@
-defmodule ScaleNative do
-  use Rustler, otp_app: :jamixir, crate: :scale
-
-  def encode_integer(_number), do: :erlang.nif_error(:nif_not_loaded)
-  def decode_integer(_vector), do: :erlang.nif_error(:nif_not_loaded)
-end
