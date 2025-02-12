05cebb3da019bd1e88eac906c36e68e78a82dcb2
Daniel Cukier <danicuki@gmail.com>
2025-02-12 19:31:07 +0000

    saving comments for later tests

--- lib/block/header/header.ex
+++ lib/block/header/header.ex
@@ -119,7 +119,12 @@ defmodule Block.Header do
     else
       case Storage.get(header.parent_hash) do
         nil ->
+          # if header.parent_hash == Hash.zero() do
+          #   :ok
+          # else
           {:error, :no_parent}
+
+        # end
 
         parent_header ->
           if parent_header.timeslot < header.timeslot do
