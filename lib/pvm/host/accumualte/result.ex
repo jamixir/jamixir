fa0e3a6ddd451aba88a9271c7c9e20b05640f3b4
luke <daiagi@gmail.com>
2025-02-18 12:33:25 +0700

    delete dead code

--- lib/pvm/host/accumualte/result.ex
+++ lib/pvm/host/accumualte/result.ex
@@ -47,14 +47,4 @@ defmodule PVM.Host.Accumulate.Result do
         context: internal.context
     }
   end
-
-  # def new(%__MODULE__{} = self, {exit_reason, %Internal{} = internal}) do
-  #   %__MODULE__{
-  #     self
-  #     | registers: internal.registers,
-  #       memory: internal.memory,
-  #       context: internal.context,
-  #       exit_reason: exit_reason
-  #   }
-  # end
 end
