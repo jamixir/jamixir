25850a7f0d65a43c295e8ea8dd0a18fd0efbeeee
luke <daiagi@gmail.com>
2025-02-23 13:13:23 +0700

    2 birds 1 stone: a. fix: del_le was consuming nor bytes then specified b. idiomaic and faster

--- lib/codec/decoder.ex
+++ lib/codec/decoder.ex
@@ -4,10 +4,13 @@ defmodule Codec.Decoder do
   """
 
   use Sizes
-  defp decode_little_endian(<<>>, _), do: 0
 
-  defp decode_little_endian(<<byte::size(8), rest::binary>>, l),
-    do: byte + decode_little_endian(rest, l - 1) * 256
+  defp decode_little_endian(binary, l) do
+    case binary do
+      <<num::size(l)-unit(8)-little, _rest::binary>> -> num
+      _ -> 0
+    end
+  end
 
   def decode_le(encoded, length), do: decode_little_endian(encoded, length)
 
