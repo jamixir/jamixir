a96facea9ed4b9735286fec83a1c446ab70faed0
Daniel Cukier <danicuki@gmail.com>
2025-03-24 20:28:21 +0000

    refactor field sum

--- lib/system/state/validator_statistics/core_statistic.ex
+++ lib/system/state/validator_statistics/core_statistic.ex
@@ -42,19 +42,26 @@ defmodule System.State.CoreStatistic do
         %__MODULE__{
           # Formula 13.8 v0.6.4
           # Formula 13.9 v0.6.4
-          imports: sum(for(r <- w.results, do: r.imports)),
-          exports: sum(for(r <- w.results, do: r.exports)),
-          extrinsic_count: sum(for(r <- w.results, do: r.extrinsic_count)),
-          extrinsic_size: sum(for(r <- w.results, do: r.extrinsic_size)),
-          gas_used: sum(for(r <- w.results, do: r.gas_used)),
+          imports: sum_field(w.results, :imports),
+          exports: sum_field(w.results, :exports),
+          extrinsic_count: sum_field(w.results, :extrinsic_count),
+          extrinsic_size: sum_field(w.results, :extrinsic_size),
+          gas_used: sum_field(w.results, :gas_used),
           bundle_size: w.specification.length,
           # Formula 13.10 v0.6.4
           da_load:
             w.specification.length +
               Constants.segment_size() * ceil(w.specification.segment_count * (65 / 64)),
-          popularity: sum(for(bits <- a_bits, do: elem(bits, c)))
+          popularity: sum_field(a_bits, c)
         }
       end
     end
   end
+
+  @spec sum_field(Enumerable.t(), atom() | non_neg_integer()) :: number()
+  def sum_field(enum, field) when is_atom(field),
+    do: Enum.reduce(enum, 0, fn item, acc -> acc + Map.get(item, field, 0) end)
+
+  def sum_field(enum, index) when is_integer(index) and index >= 0,
+    do: Enum.reduce(enum, 0, fn item, acc -> acc + elem(item, index) end)
 end
