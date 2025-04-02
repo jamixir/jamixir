fc43d0f76b5bd672f08f585e40b45035ca28ef0a
Daniel Cukier <danicuki@gmail.com>
2025-04-02 20:59:21 +0100

    announce audit - ce 144

---
+++ lib/system/audit/audit_announcement.ex
@@ -0,0 +1,17 @@
+defmodule System.Audit.AuditAnnouncement do
+  use Codec.Encoder
+
+  @type t :: %__MODULE__{
+          tranche: non_neg_integer(),
+          announcements: list({non_neg_integer(), Types.hash()}),
+          header_hash: Types.hash(),
+          signature: Types.signature(),
+          evidence: Types.bandersnatch_signature() | list(NoShow.t())
+        }
+
+  defstruct tranche: 0,
+            announcements: [],
+            header_hash: nil,
+            signature: nil,
+            evidence: nil
+end
