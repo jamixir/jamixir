3f7070293459e4b7f324e1da4b0ae5aa845e10a0
Daniel Cukier <danicuki@gmail.com>
2025-06-04 22:50:52 +0100

    refactor traces and testnet tests to use same infrastructure.

--- test/test_vectors/traces_test.exs
+++ test/test_vectors/traces_test.exs
@@ -19,108 +19,132 @@ defmodule TracesTest do
   def trace_enabled?, do: System.get_env("PVM_TRACE") == "true"
 
   @ignore_fields []
-  @user "davxy"
-  @repo "jam-test-vectors"
 
-  def block_path(mode), do: "traces/#{mode}"
+  def traces_path(mode), do: "traces/#{mode}"
+  def testnet_path(mode), do: "data/#{mode}/state_transitions"
 
-  # "reports-l0"
-  describe "blocks and states" do
-    for mode <- ["fallback", "safrole", "reports-l0"] do
-      @tag mode: mode
-      @tag timeout: :infinity
-      @tag :slow
-      test "#{mode} mode block import", %{mode: mode} do
-        {failed_blocks, _} =
-          for block_number <- 1..42, reduce: {[], nil} do
-            {failed_blocks, pre_state} ->
-              if trace_enabled?() do
-                System.put_env("TRACE_NAME", "block-#{block_number}")
-              end
+  oficial = %{
+    user: "davxy",
+    repo: "jam-test-vectors",
+    branch: "master",
+    path: &__MODULE__.traces_path/1,
+    block_range: 1..42,
+    modes: ["fallback", "safrole", "reports-l0"]
+  }
 
-              Logger.info("🧱 Processing block #{block_number}")
-              file = String.pad_leading("#{block_number}", 8, "0")
+  jam_duna = %{
+    user: "jamixir",
+    repo: "jamtestnet",
+    branch: "master",
+    path: &__MODULE__.testnet_path/1,
+    block_range: 12..47,
+    modes: ["generic", "assurances", "orderedaccumulation"]
+  }
 
-              {:ok, block_json} =
-                case fetch_and_parse_json(
-                       "#{file}.json",
-                       block_path(mode),
-                       @user,
-                       @repo,
-                       "traces"
-                     ) do
-                  {:error, _} ->
-                    throw("Error fetching #{file}.json")
+  def block_path(config, mode), do: "#{config[:path]}/#{mode}"
 
-                  any ->
-                    any
+  # add jam_duna to the list of configs if you want to test it
+  configs = [oficial]
+
+  for config <- configs do
+    describe "blocks and states" do
+      for mode <- config[:modes] do
+        @tag mode: mode
+        @tag timeout: :infinity
+        @tag config: config
+        @tag :slow
+        test "#{mode} mode block import", %{mode: mode, config: config} do
+          {failed_blocks, _} =
+            for block_number <- config[:block_range], reduce: {[], nil} do
+              {failed_blocks, pre_state} ->
+                if trace_enabled?() do
+                  System.put_env("TRACE_NAME", "block-#{block_number}")
                 end
 
-              parent_hash = JsonDecoder.from_json(block_json[:block][:header][:parent])
+                Logger.info("🧱 Processing block #{block_number}")
+                file = String.pad_leading("#{block_number}", 8, "0")
 
-              unless Storage.get(parent_hash),
-                do: Storage.put(parent_hash, build(:header, timeslot: 0))
+                {:ok, block_json} =
+                  case fetch_and_parse_json(
+                         "#{file}.json",
+                         config[:path].(mode),
+                         config[:user],
+                         config[:repo],
+                         config[:branch]
+                       ) do
+                    {:error, _} ->
+                      throw("Error fetching #{file}.json")
 
-              pre_state_trie = Trie.from_json(block_json[:pre_state][:keyvals])
-
-              pre_state = if pre_state, do: pre_state, else: Trie.trie_to_state(pre_state_trie)
-
-              block = Block.from_json(block_json[:block])
-              expected_trie = Trie.from_json(block_json[:post_state][:keyvals])
-
-              case State.add_block(pre_state, block) do
-                {:ok, new_state} ->
-                  Storage.put(block.header)
-                  Logger.info("🔄 State Updated successfully")
-                  expected_state = Trie.trie_to_state(expected_trie)
-
-                  #
-                  Logger.info("🔍 Comparing state")
-                  # uncomment to delete statistics from state trie
-                  # |> Map.delete(<<13, 0::30*8>>)
-                  trie1 = Trie.serialize(new_state)
-                  # |> Map.delete(<<13, 0::30*8>>)
-                  trie2 = expected_trie
-
-                  if trie1 != trie2 do
-                    failed_fields =
-                      for field <- Utils.list_struct_fields(System.State), reduce: [] do
-                        acc ->
-                          if field in @ignore_fields do
-                            acc
-                          else
-                            expected = Map.get(expected_state, field)
-                            new = Map.get(new_state, field)
-
-                            if expected != new do
-                              Logger.info("❌ Field #{field} mismatch")
-                              Logger.info("Expected: #{inspect(expected)}")
-                              Logger.info("New: #{inspect(new)}")
-                              acc ++ [field]
-                            else
-                              acc
-                            end
-                          end
-                      end
-
-                    if failed_fields == [] do
-                      {failed_blocks, new_state}
-                    else
-                      {failed_blocks ++ [{block_number, failed_fields}], nil}
-                    end
-                  else
-                    {failed_blocks, new_state}
+                    any ->
+                      any
                   end
 
-                {:error, _, error} ->
-                  Logger.info("#{ANSI.red()} Error processing block: #{error}")
+                parent_hash = JsonDecoder.from_json(block_json[:block][:header][:parent])
 
-                  {failed_blocks ++ [{block_number, error}], nil}
-              end
-          end
+                unless Storage.get(parent_hash),
+                  do: Storage.put(parent_hash, build(:header, timeslot: 0))
 
-        assert failed_blocks == []
-        Logger.info("🎉 All blocks and states are correct")
+                pre_state_trie = Trie.from_json(block_json[:pre_state][:keyvals])
+
+                pre_state = if pre_state, do: pre_state, else: Trie.trie_to_state(pre_state_trie)
+
+                block = Block.from_json(block_json[:block])
+                expected_trie = Trie.from_json(block_json[:post_state][:keyvals])
+
+                case State.add_block(pre_state, block) do
+                  {:ok, new_state} ->
+                    Storage.put(block.header)
+                    Logger.info("🔄 State Updated successfully")
+                    expected_state = Trie.trie_to_state(expected_trie)
+
+                    #
+                    Logger.info("🔍 Comparing state")
+                    # uncomment to delete statistics from state trie
+                    # |> Map.delete(<<13, 0::30*8>>)
+                    trie1 = Trie.serialize(new_state)
+                    # |> Map.delete(<<13, 0::30*8>>)
+                    trie2 = expected_trie
+
+                    if trie1 != trie2 do
+                      failed_fields =
+                        for field <- Utils.list_struct_fields(System.State), reduce: [] do
+                          acc ->
+                            if field in @ignore_fields do
+                              acc
+                            else
+                              expected = Map.get(expected_state, field)
+                              new = Map.get(new_state, field)
+
+                              if expected != new do
+                                Logger.info("❌ Field #{field} mismatch")
+                                Logger.info("Expected: #{inspect(expected)}")
+                                Logger.info("New: #{inspect(new)}")
+                                acc ++ [field]
+                              else
+                                acc
+                              end
+                            end
+                        end
+
+                      if failed_fields == [] do
+                        {failed_blocks, new_state}
+                      else
+                        {failed_blocks ++ [{block_number, failed_fields}], nil}
+                      end
+                    else
+                      {failed_blocks, new_state}
+                    end
+
+                  {:error, _, error} ->
+                    Logger.info("#{ANSI.red()} Error processing block: #{error}")
+
+                    {failed_blocks ++ [{block_number, error}], nil}
+                end
+            end
+
+          assert failed_blocks == []
+          Logger.info("🎉 All blocks and states are correct")
+        end
       end
     end
   end
