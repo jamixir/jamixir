c78b5bf045103dfb94cc0facefbadb2a77bcf728
Daniel Cukier <danicuki@gmail.com>
2025-06-05 15:49:19 +0100

    removed old files

--- implemented_formulas.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-grep -o "Formula ([0-9ABCDEFGH]\+[.][0-9]\+)" -R --include \*.{ex,exs} * | awk -F'[()]' '{print $2}' | sort -V |uniq
--- import_blocks.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./ctest.sh test/test_vectors/testnet_block_importer_test.exs |pv -L 500 -q
