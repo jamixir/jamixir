e0fcab684d23814362ba925c33bf5c3f8bf4a058
daiagi <daiagi@gmail.com>
2025-06-18 14:12:57 +0700

    fix: assurances test vectors

--- test/test_vectors/test_vector_util.ex
+++ test/test_vectors/test_vector_util.ex
@@ -120,7 +120,7 @@ defmodule TestVectorUtil do
 
   def accumulate_mock_return,
     do: %{
-      lastaccout: <<>>,
+      lastaccouts: [],
       authorizer_queue: [],
       services: %{},
       next_validators: [],
