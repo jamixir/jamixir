20f58dc42ecfe75cce29efba81f83da68bad4f0a
Luke Fishman <daiagi@gmail.com>
2025-06-19 15:38:15 +0700

    Update comment of Formula 11.33
    Co-authored-by: Daniel Cukier <danicuki@gmail.com>

--- lib/block/extrinsic/guarantees/guarantee.ex
+++ lib/block/extrinsic/guarantees/guarantee.ex
@@ -191,7 +191,7 @@ defmodule Block.Extrinsic.Guarantee do
   end
 
   # Formula (11.33) v0.6.8
-  # ∀x ∈ x ∶ ∃y ∈ β ∶ xa = yh ∧ xs = ys ∧ xb = yb
+# ∀x ∈ x ∶ ∃y ∈ β†H ∶ xa = yh ∧ xs = ys ∧ xb = yb
   mockable validate_anchor_block(guarantees, %RecentHistory{} = beta, prior_state_root) do
     beta_dagger = RecentHistory.update_latest_state_root(beta, prior_state_root)
 
