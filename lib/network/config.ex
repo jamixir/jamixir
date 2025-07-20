aa0f57c02382f0c8154c523d5780efee6583e5dc
daiagi <daiagi@gmail.com>
2025-07-20 15:58:24 +0700

    alpn protocl id

--- lib/network/config.ex
+++ lib/network/config.ex
@@ -1,5 +1,8 @@
 defmodule Network.Config do
   alias Network.CertUtils
+  alias Jamixir.Genesis
+  import Util.Hex, only: [encode16: 1]
+  @protocol_version "0"
 
   @default_peer_config [
     init_mode: :initiator,
@@ -8,28 +11,38 @@ defmodule Network.Config do
     timeout: 5_000
   ]
 
-  @fixed_quicer_opts [
-    alpn: [~c"jamnp-s/V/H"],
-    peer_bidi_stream_count: Constants.validator_count(),
-    peer_unidi_stream_count: 100,
-    versions: [:"tlsv1.3"],
-    verify: :none,
-    conn_acceptors: 4,
-    # TODO: this is hack to prevent quicer from closing the connection when the peer is not sending any data
-    # instead we shoudl intiate up stream and indeed shut dow connection if nothing is moving on them
-    # the quicer default is 30 seconds
-    idle_timeout_ms: 0
-  ]
+  def fixed_quicer_opts do
+    [
+      alpn: [~c"#{alpn_protocol_identifier()}"],
+      peer_bidi_stream_count: Constants.validator_count(),
+      peer_unidi_stream_count: 100,
+      versions: [:"tlsv1.3"],
+      verify: :none,
+      conn_acceptors: 4,
+      # TODO: this is hack to prevent quicer from closing the connection when the peer is not sending any data
+      # instead we shoudl intiate up stream and indeed shut dow connection if nothing is moving on them
+      # the quicer default is 30 seconds
+      idle_timeout_ms: 0
+    ]
+  end
 
-  @default_quicer_opts [
-                         certfile: ~c"#{CertUtils.certfile()}",
-                         keyfile: ~c"#{CertUtils.keyfile()}"
-                       ] ++ @fixed_quicer_opts
+  def default_quicer_opts do
+    [
+      certfile: ~c"#{CertUtils.certfile()}",
+      keyfile: ~c"#{CertUtils.keyfile()}"
+    ] ++ fixed_quicer_opts()
+  end
 
   @default_stream_opts %{active: true}
 
-  def default_quicer_opts, do: @default_quicer_opts
   def default_peer_config, do: @default_peer_config
 
   def default_stream_opts, do: @default_stream_opts
+
+  def alpn_protocol_identifier do
+    <<first_8_nibbles::4-binary, _rest::binary>> = Genesis.genesis_header_hash()
+    "jamnp-s/#{@protocol_version}/#{encode16(first_8_nibbles)}"
+  end
+
+  def alpn_protocol_identifier_builder, do: "#{alpn_protocol_identifier()}/builder"
 end
---
+++ test/network/config_test.exs
@@ -0,0 +1,38 @@
+defmodule Network.ConfigTest do
+  use ExUnit.Case
+  alias Network.Config
+  import Util.Hex, only: [encode16: 1]
+
+  describe "alpn_protocol_identifier/0" do
+    test "generates correct protocol identifier format" do
+      identifier = Config.alpn_protocol_identifier()
+
+      assert String.starts_with?(identifier, "jamnp-s/0/")
+      # "jamnp-s/0/" (10) + 8 hex chars
+      assert String.length(identifier) == 18
+
+      hash_part = String.slice(identifier, 10..-1)
+
+      assert String.match?(hash_part, ~r/^[0-9a-f]{8}$/)
+    end
+
+    test "uses genesis header hash first 8 nibbles" do
+      # Get the genesis header hash
+      genesis_hash = Jamixir.Genesis.genesis_header_hash()
+      <<first_8_nibbles::4-binary, _rest::binary>> = genesis_hash
+
+      identifier = Config.alpn_protocol_identifier()
+
+      assert String.slice(identifier, 10..-1) == encode16(first_8_nibbles)
+    end
+  end
+
+  describe "alpn_protocol_identifier_builder/0" do
+    test "adds /builder suffix to base identifier" do
+      base_identifier = Config.alpn_protocol_identifier()
+      builder_identifier = Config.alpn_protocol_identifier_builder()
+
+      assert builder_identifier == "#{base_identifier}/builder"
+    end
+  end
+end
