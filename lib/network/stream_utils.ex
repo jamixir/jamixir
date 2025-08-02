12b4bee986ad591466b801cd3614004870232862
Daniel Cukier <danicuki@gmail.com>
2025-08-02 11:42:54 +0100

    replace switch case with dict lookup

--- lib/network/stream_utils.ex
+++ lib/network/stream_utils.ex
@@ -10,27 +10,26 @@ defmodule Network.StreamUtils do
 
   def format_stream_ref(other), do: inspect(other)
 
-  def protocol_description(protocol_id) when is_integer(protocol_id) do
-    case protocol_id do
-      0 -> "block_announce"
-      128 -> "req_blocks"
-      129 -> "req_state"
-      131 -> "ticket_proxy"
-      132 -> "ticket_validator"
-      133 -> "work_package"
-      134 -> "work_bundle"
-      135 -> "guarantee"
-      136 -> "work_report"
-      137 -> "req_wp_shard"
-      138 -> "req_audit_shard"
-      139 -> "req_shards"
-      140 -> "req_shards_just"
-      141 -> "assurance"
-      142 -> "announce_preimage"
-      143 -> "get_preimage"
-      144 -> "announce_audit"
-      145 -> "announce_judgement"
-      _ -> "unknown_#{protocol_id}"
-    end
-  end
+  @protocol_desc %{
+    0 => "block_announce",
+    128 => "req_blocks",
+    129 => "req_state",
+    131 => "ticket_proxy",
+    132 => "ticket_validator",
+    133 => "work_package",
+    134 => "work_bundle",
+    135 => "guarantee",
+    136 => "work_report",
+    137 => "req_wp_shard",
+    138 => "req_audit_shard",
+    139 => "req_shards",
+    140 => "req_shards_just",
+    141 => "assurance",
+    142 => "announce_preimage",
+    143 => "get_preimage",
+    144 => "announce_audit",
+    145 => "announce_judgement"
+  }
+  def protocol_description(protocol_id),
+    do: Map.get(@protocol_desc, protocol_id, "unknown_#{protocol_id}")
 end
