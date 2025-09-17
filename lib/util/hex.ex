08196745d4bed62439f679eebde1efb3abe85953
Daniel Cukier <danicuki@gmail.com>
2025-09-17 17:01:00 +0100

    remove memoize for base 16 encoding and decoding

--- lib/util/hex.ex
+++ lib/util/hex.ex
@@ -1,24 +1,22 @@
 defmodule Util.Hex do
-  use Memoize
   @doc """
   Decodes a hex string that may optionally start with "0x"
   """
-  defmemo decode16(hex_str, opts \\ [case: :lower])
-  defmemo decode16("0x" <> hex_str, opts), do: Base.decode16(hex_str, opts)
-  defmemo decode16(hex_str, opts), do: Base.decode16(hex_str, opts)
+  def decode16(hex_str, opts \\ [case: :lower])
+  def decode16("0x" <> hex_str, opts), do: Base.decode16(hex_str, opts)
+  def decode16(hex_str, opts), do: Base.decode16(hex_str, opts)
 
-  defmemo decode16!(hex_str, opts \\ [case: :lower])
-  defmemo decode16!("0x" <> hex_str, opts), do: Base.decode16!(hex_str, opts)
-  defmemo decode16!(hex_str, opts), do: Base.decode16!(hex_str, opts)
+  def decode16!(hex_str, opts \\ [case: :lower])
+  def decode16!("0x" <> hex_str, opts), do: Base.decode16!(hex_str, opts)
+  def decode16!(hex_str, opts), do: Base.decode16!(hex_str, opts)
+  def encode16(binary, opts \\ [])
 
-  defmemo encode16(binary, opts \\ [])
-
-  defmemo encode16(binary, case: casing, prefix: prefix) do
+  def encode16(binary, case: casing, prefix: prefix) do
     hex = Base.encode16(binary, case: casing)
     if prefix, do: "0x" <> hex, else: hex
   end
 
-  defmemo encode16(binary, opts) do
+  def encode16(binary, opts) do
     casing = Keyword.get(opts, :case, :lower)
     prefix = Keyword.get(opts, :prefix, false)
     encode16(binary, case: casing, prefix: prefix)
