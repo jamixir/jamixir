6671869e4f1e1ac9f5a7dabf687f39f1be18eb57
Daniel Cukier <danicuki@gmail.com>
2026-01-07 12:50:13 +0000

    node 0 always on debug and rpc enable

--- run_node.sh
+++ run_node.sh
@@ -2,6 +2,14 @@
 NODE=$1
 PORT=$(($NODE+10001))
 VERSION=$(grep -o 'version: "[^"]*"' mix.exs | sed 's/version: "//;s/"//')
+
+if [ "$NODE" -eq "0" ]; then
+  RPC=--rpc
+  LOG=--log=debug
+else
+  RPC=
+fi
+
 PRIV_DIR=./lib/jamixir-${VERSION}/priv/
 cd _build/tiny/rel/jamixir
-./jamixir run -k ${PRIV_DIR}/keys/$NODE.json --port $PORT --db db/db${NODE} $2
+./jamixir run -k ${PRIV_DIR}/keys/$NODE.json ${RPC} ${LOG} --port $PORT --db db/db${NODE} $2
