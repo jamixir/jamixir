0f502f452a9ddbf88a547976dbdf28f1bfc4a527
Daniel Cukier <danicuki@gmail.com>
2026-01-09 13:25:31 +0000

    remove dependency on memory on vm results

--- lib/pvm/host/refine/result.ex
+++ lib/pvm/host/refine/result.ex
@@ -5,43 +5,33 @@ defmodule PVM.Host.Refine.Result.Internal do
   @type t() :: %__MODULE__{
           exit_reason: :continue | :out_of_gas | :panic,
           registers: Registers.t(),
-          memory: Memory.t(),
           context: Context.t()
         }
 
   defstruct exit_reason: :continue,
             registers: %Registers{},
-            memory: %Memory{},
             context: %Context{}
 end
 
 defmodule PVM.Host.Refine.Result do
-  alias PVM.Host.Refine.Result.Internal
-  alias PVM.{Memory, Registers}
   alias PVM.Host.Refine.Context
+  alias PVM.Host.Refine.Result.Internal
+  alias PVM.Registers
 
   @type t() :: %__MODULE__{
           exit_reason: :continue | :out_of_gas | :panic,
           gas: non_neg_integer(),
           registers: Registers.t(),
-          memory: Memory.t(),
           context: Context.t()
         }
 
-  defstruct [
-    :exit_reason,
-    :gas,
-    :registers,
-    :memory,
-    :context
-  ]
+  defstruct [:exit_reason, :gas, :registers, :context]
 
   def new(%__MODULE__{} = self, %Internal{} = internal) do
     %__MODULE__{
       self
       | exit_reason: internal.exit_reason,
         registers: internal.registers,
-        memory: internal.memory,
         context: internal.context
     }
   end
