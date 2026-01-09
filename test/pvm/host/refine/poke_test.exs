500541553ae71708c8249b95abba8f54fe9215f4
Daniel Cukier <danicuki@gmail.com>
2026-01-09 11:19:23 +0000

    poke refine host call using rust pvm

--- test/pvm/host/refine/poke_test.exs
+++ test/pvm/host/refine/poke_test.exs
@@ -1,9 +1,10 @@
 defmodule PVM.Host.Refine.PokeTest do
   use ExUnit.Case
   alias PVM.Host.Refine
-  alias PVM.{Memory, Host.Refine.Context, Integrated, Registers, PreMemory}
+  alias PVM.{Host.Refine.Context, Integrated, Registers}
   import PVM.Constants.HostCallResult
   import PVM.Memory.Constants
+  import Pvm.Native
 
   defp a_0, do: min_allowed_address()
 
@@ -11,37 +12,27 @@ defmodule PVM.Host.Refine.PokeTest do
     setup do
       test_data = String.duplicate("A", 32)
 
-      pvm_memory =
-        PreMemory.init_nil_memory()
-        |> PreMemory.set_access(a_0(), 1, :read)
-        |> PreMemory.write(a_0(), test_data)
-        |> PreMemory.finalize()
+      memory_ref = build_memory()
+      set_memory_access(memory_ref, a_0(), byte_size(test_data), 3)
+      memory_write(memory_ref, a_0(), test_data)
 
-      machine_memory =
-        PreMemory.init_nil_memory()
-        |> PreMemory.set_access(a_0(), 1, :write)
-        |> PreMemory.finalize()
+      machine_memory_ref = build_memory()
+      set_memory_access(machine_memory_ref, a_0(), byte_size(test_data), 3)
 
       machine = %Integrated{
-        memory: machine_memory,
+        memory: machine_memory_ref,
         program: "program"
       }
 
       context = %Context{m: %{1 => machine}}
 
       # r7: machine ID, r8: source offset, r9: dest offset, r10: length
-      registers =
-        Registers.new(%{
-          7 => 1,
-          8 => a_0(),
-          9 => a_0(),
-          10 => byte_size(test_data)
-        })
+      registers = Registers.new(%{7 => 1, 8 => a_0(), 9 => a_0(), 10 => byte_size(test_data)})
 
       gas = 100
 
       {:ok,
-       memory: pvm_memory,
+       memory_ref: memory_ref,
        context: context,
        machine: machine,
        gas: gas,
@@ -50,7 +41,7 @@ defmodule PVM.Host.Refine.PokeTest do
     end
 
     test "returns WHO when machine doesn't exist", %{
-      memory: memory,
+      memory_ref: memory_ref,
       context: context,
       gas: gas,
       registers: registers
@@ -59,11 +50,8 @@ defmodule PVM.Host.Refine.PokeTest do
       registers = %{registers | r: put_elem(registers.r, 7, 999)}
       who = who()
 
-      assert %{
-               exit_reason: :continue,
-               registers: registers_,
-               context: ^context
-             } = Refine.poke(gas, registers, memory, context)
+      assert %{exit_reason: :continue, registers: registers_, context: ^context} =
+               Refine.poke(gas, registers, memory_ref, context)
 
       assert registers_[7] == who
     end
@@ -74,57 +62,44 @@ defmodule PVM.Host.Refine.PokeTest do
       registers: registers
     } do
       # Make source memory unreadable
-      memory = Memory.set_access(%Memory{}, registers[8], registers[10], nil)
+      memory_ref = build_memory()
+      set_memory_access(memory_ref, registers[8], registers[10], 0)
 
-      assert %{
-               exit_reason: :panic,
-               registers: ^registers,
-               context: ^context
-             } = Refine.poke(gas, registers, memory, context)
+      assert %{exit_reason: :panic, registers: ^registers, context: ^context} =
+               Refine.poke(gas, registers, memory_ref, context)
     end
 
     test "returns OOB when destination memory not writable", %{
-      memory: memory,
+      memory_ref: memory_ref,
       context: context,
       machine: machine,
       gas: gas,
       registers: registers
     } do
       # Make machine memory unwritable
-      machine = %{
-        machine
-        | memory: Memory.set_access(machine.memory, registers[9], registers[10], :read)
-      }
+      set_memory_access(machine.memory, registers[9], registers[10], 1)
 
-      context = %{context | m: %{1 => machine}}
-
-      assert %{
-               exit_reason: :continue,
-               registers: registers_,
-               context: ^context
-             } = Refine.poke(gas, registers, memory, context)
+      assert %{exit_reason: :continue, registers: registers_, context: ^context} =
+               Refine.poke(gas, registers, memory_ref, context)
 
       assert registers_[7] == oob()
     end
 
     test "successful poke with valid parameters", %{
-      memory: memory,
+      memory_ref: memory_ref,
       context: context,
       gas: gas,
       registers: registers,
       test_data: test_data
     } do
-      assert %{
-               exit_reason: :continue,
-               registers: registers_,
-               context: context_
-             } = Refine.poke(gas, registers, memory, context)
+      assert %{exit_reason: :continue, registers: registers_, context: context_} =
+               Refine.poke(gas, registers, memory_ref, context)
 
       assert registers_[7] == ok()
 
       # Verify data was copied correctly to machine memory
       machine = Map.get(context_.m, 1)
-      assert Memory.read!(machine.memory, registers[9], registers[10]) == test_data
+      {:ok, ^test_data} = memory_read(machine.memory, registers[9], registers[10])
     end
   end
 end
