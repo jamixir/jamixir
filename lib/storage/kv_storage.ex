44384225634564855cf89a17fbb27c67668a9c69
Daniel Cukier <danicuki@gmail.com>
2026-01-27 09:26:18 +0000

    better cache maintance strategy

--- lib/storage/kv_storage.ex
+++ lib/storage/kv_storage.ex
@@ -1,5 +1,6 @@
 defmodule KVStorage do
   @cache_table :kv_cache
+  @cache_order :kv_cache_order
   @cache_meta :kv_cache_meta
   # Max entries before eviction (configurable via Jamixir.config()[:kv_cache_max_entries])
   @default_max_entries 100_000
@@ -26,7 +27,7 @@ defmodule KVStorage do
 
   def get(key) do
     case :ets.lookup(@cache_table, key) do
-      [{^key, value}] ->
+      [{^key, _seq, value}] ->
         value
 
       [] ->
@@ -43,14 +44,25 @@ defmodule KVStorage do
   end
 
   def remove(key) do
-    :ets.delete(@cache_table, key)
+    case :ets.lookup(@cache_table, key) do
+      [{^key, seq, _value}] ->
+        :ets.delete(@cache_table, key)
+        :ets.delete(@cache_order, seq)
+        update_size(-1)
+
+      [] ->
+        :ok
+    end
+
     PersistStorage.delete(key)
     :ok
   end
 
   def remove_all do
     :ets.delete_all_objects(@cache_table)
+    :ets.delete_all_objects(@cache_order)
     :ets.insert(@cache_meta, {:size, 0})
+    :ets.insert(@cache_meta, {:seq, 0})
     PersistStorage.clear()
   end
 
@@ -58,20 +70,29 @@ defmodule KVStorage do
 
   defp init_cache do
     if :ets.whereis(@cache_table) == :undefined do
+      # Main cache: key -> {key, seq, value}
       :ets.new(@cache_table, [:set, :public, :named_table, {:read_concurrency, true}])
+      # Order tracking: seq -> key (ordered_set for FIFO eviction)
+      :ets.new(@cache_order, [:ordered_set, :public, :named_table])
       :ets.new(@cache_meta, [:set, :public, :named_table])
       :ets.insert(@cache_meta, {:size, 0})
+      :ets.insert(@cache_meta, {:seq, 0})
     end
   end
 
   defp cache_put(key, value) do
-    is_new = :ets.insert_new(@cache_table, {key, value})
+    case :ets.lookup(@cache_table, key) do
+      [{^key, _old_seq, _old_value}] ->
+        # Update existing - keep same seq (don't change order)
+        :ets.update_element(@cache_table, key, {3, value})
 
-    if is_new do
-      new_size = :ets.update_counter(@cache_meta, :size, 1)
-      maybe_evict(new_size)
-    else
-      :ets.insert(@cache_table, {key, value})
+      [] ->
+        # New entry - assign sequence number
+        seq = :ets.update_counter(@cache_meta, :seq, 1)
+        :ets.insert(@cache_table, {key, seq, value})
+        :ets.insert(@cache_order, {seq, key})
+        new_size = :ets.update_counter(@cache_meta, :size, 1)
+        maybe_evict(new_size)
     end
   end
 
@@ -79,23 +100,30 @@ defmodule KVStorage do
     max_entries = max_cache_entries()
 
     if current_size > max_entries do
-      # Evict ~20% of entries (random eviction - simple and fast)
+      # Evict oldest 20% (FIFO)
       evict_count = div(max_entries, 5)
-      evict_random(evict_count)
+      evict_oldest(evict_count)
     end
   end
 
-  defp evict_random(count) do
-    do_evict(:ets.first(@cache_table), count, 0)
-  end
+  defp evict_oldest(count) do
+    # Find the cutoff sequence number (oldest N entries)
+    case :ets.select(@cache_order, [{{:"$1", :_}, [], [:"$1"]}], count) do
+      {seqs, _continuation} when seqs != [] ->
+        max_seq = Enum.max(seqs)
 
-  defp do_evict(:"$end_of_table", _count, deleted), do: update_size(-deleted)
-  defp do_evict(_key, count, deleted) when deleted >= count, do: update_size(-deleted)
+        # Bulk delete from order table: all entries with seq <= max_seq
+        deleted_order =
+          :ets.select_delete(@cache_order, [{{:"$1", :_}, [{:"=<", :"$1", max_seq}], [true]}])
 
-  defp do_evict(key, count, deleted) do
-    next_key = :ets.next(@cache_table, key)
-    :ets.delete(@cache_table, key)
-    do_evict(next_key, count, deleted + 1)
+        # Bulk delete from cache table: all entries with seq <= max_seq
+        :ets.select_delete(@cache_table, [{{:_, :"$1", :_}, [{:"=<", :"$1", max_seq}], [true]}])
+
+        update_size(-deleted_order)
+
+      _ ->
+        :ok
+    end
   end
 
   defp update_size(delta) do
