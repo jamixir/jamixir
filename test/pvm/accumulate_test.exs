edeccd62fe64c91bae0b9b3961a1861ca2f6a9ab
Daniel Cukier <danicuki@gmail.com>
2026-02-03 09:52:50 +0000

    full accumulation test

--- test/pvm/accumulate_test.exs
+++ test/pvm/accumulate_test.exs
@@ -1,5 +1,6 @@
 defmodule PVM.AccumulateTest do
   use ExUnit.Case
+  alias Jamixir.ChainSpec
   alias System.AccumulationResult
   alias System.State.{Accumulation, ServiceAccount}
   alias PVM.{Accumulate, Accumulate.Operand}
@@ -72,6 +73,36 @@ defmodule PVM.AccumulateTest do
       assert is_nil(acc_result.output)
       # Some gas was consumed
       assert acc_result.gas_used < 1000
+    end
+  end
+
+  describe "accumulation integration tests" do
+    test "bootstrap service - vm new service" do
+      state = ChainSpec.bootstrap_state()
+
+      service = state.services[0]
+
+      service = %{
+        service
+        | preimage_storage_p: AccumulationTestHelper.bootstrap_storage_p(),
+          storage: %{
+            HashedKeysMap.new()
+            | hashed_map: AccumulationTestHelper.bootstrap_storage(),
+              octets_in_storage: 426_358,
+              items_in_storage: 11
+          }
+      }
+
+      accumulation = %Accumulation{services: %{0 => service}}
+
+      inputs_hex =
+        "0x01003e0e636827f55a836ce65b0fbfa73a46edc5d76bf9d94ba1223c987093c7061900000000000000000000000000000000000000000000000000000000000000002357426f2313559a271d6782dc00197b379f79cbe3c6a1e72f61f7b592c509f8e6a04b842d4eb6b03dbd60ad97037898359ccf465c8f8e31985ee18c281337a2e08096980080c301000806a2111844d41615be6eb7760647537b8e3f5a42f96921930913255ab4d1bdc32a04000000000040420f000000000040420f000000000000ca9a3b0000000000ffffffffffffff7f000000006f0a56dd38bc100f39d69f7eeb973dc17bdefb4cf92f999c6108b2979c37a2e900000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+
+      inputs_bin = Util.Hex.decode16!(inputs_hex)
+
+      {inputs, _} = Accumulation.decode_inputs(inputs_bin)
+
+      PVM.accumulate(accumulation, 100, 0, 7_000_000_000, inputs, %{n0_: Hash.random()})
     end
   end
 end
