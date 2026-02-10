8c0d5b1893edb278927ca28dc8af3442954dd8d8
daiagi <daiagi@gmail.com>
2026-02-10 14:13:07 +0700

    NIF interface for new child vm

--- native/pvm/src/lib.rs
+++ native/pvm/src/lib.rs
@@ -1,15 +1,17 @@
 pub mod atoms;
+pub mod child_vm;
 pub mod context;
 pub mod execution;
 pub mod memory;
 pub mod nif_functions;
 pub mod nif_types;
 
+use crate::child_vm::ChildVmResource;
 use crate::memory::MemoryResource;
 use rustler::{Env, Term};
 
 rustler::init!("Elixir.Pvm.Native", load = load);
 
 fn load(env: Env, _info: Term) -> bool {
-    env.register::<MemoryResource>().is_ok()
+    env.register::<MemoryResource>().is_ok() && env.register::<ChildVmResource>().is_ok()
 }
